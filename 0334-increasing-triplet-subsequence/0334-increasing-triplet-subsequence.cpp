e717660c403f6a0b3ce1ec30d319ba3338a8b5fe
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2023-08-28 09:07:35 +0530

    Time: 120 ms (48.97%), Space: 121.3 MB (5.06%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -6,20 +6,29 @@ public:
         vector<int> larger(n);
 
         smaller[0]=nums[0];
+        larger[n-1]=nums[n-1];
 
         for(int i=1;i<n;i++)
+        {
             if(nums[i]<smaller[i-1])
                 smaller[i]=nums[i];
             else
                 smaller[i]=smaller[i-1];
-        
-        larger[n-1]=nums[n-1];
 
-        for(int i=n-2;i>=0;i--)
-            if(nums[i]>larger[i+1])
-                larger[i]=nums[i];
+            if(nums[n-i-1]>larger[n-i-1+1])
+                larger[n-i-1]=nums[n-i-1];
             else
-                larger[i]=larger[i+1];
+                larger[n-i-1]=larger[n-i-1+1];
+
+        }
+        
+        
+
+        // for(int i=n-2;i>=0;i--)
+        //     if(nums[i]>larger[i+1])
+        //         larger[i]=nums[i];
+        //     else
+        //         larger[i]=larger[i+1];
 
         for(int i=1;i<n-1;i++)
             if(smaller[i-1]<nums[i]&&larger[i+1]>nums[i])
