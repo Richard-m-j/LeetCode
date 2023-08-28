04e984aec98002f24ef5e2d80f4cb7b58572c279
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2023-08-28 09:19:27 +0530

    Time: 83 ms (94.66%), Space: 111.8 MB (41.45%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -1,37 +1,15 @@
 class Solution {
 public:
     bool increasingTriplet(vector<int>& nums) {
-        int n=nums.size();
-        vector<int> smaller(n);
-        vector<int> larger(n);
+        int a=INT_MAX;
+        int b=INT_MAX;
 
-        smaller[0]=nums[0];
-        larger[n-1]=nums[n-1];
-
-        for(int i=1;i<n;i++)
-        {
-            if(nums[i]<smaller[i-1])
-                smaller[i]=nums[i];
-            else
-                smaller[i]=smaller[i-1];
-
-            if(nums[n-i-1]>larger[n-i-1+1])
-                larger[n-i-1]=nums[n-i-1];
-            else
-                larger[n-i-1]=larger[n-i-1+1];
-
-        }
-        
-        
-
-        // for(int i=n-2;i>=0;i--)
-        //     if(nums[i]>larger[i+1])
-        //         larger[i]=nums[i];
-        //     else
-        //         larger[i]=larger[i+1];
-
-        for(int i=1;i<n-1;i++)
-            if(smaller[i-1]<nums[i]&&larger[i+1]>nums[i])
+        for(auto num: nums)
+            if(a>=num)
+                a=num;
+            else if(b>=num)
+                b=num;
+            else 
                 return true;
         return false;
     }
