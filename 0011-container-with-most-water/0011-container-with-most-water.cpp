d92ed159c4f21d64b8a479e1fb19aacace6fe99a
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2023-08-29 14:38:42 +0530

    Time: 51 ms (99.12%), Space: 59.5 MB (5.33%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -13,33 +13,22 @@ public:
         ios_base::sync_with_stdio(false);
 
         int n = height.size();
-        int maxVol = volume(height[0], height[n - 1], n - 1);
+        int maxVol = 0;
         int left = 0, right = n - 1;
         int shorter,current;
         while (left < right)
         {
+            shorter = min(height[left],height[right]);
+            current = volume(height[left], height[right], right - left);
+            if (current > maxVol)
+                maxVol = current;
+            
             if (height[left] < height[right])
-            {
-                shorter = height[left];
-                current = volume(height[left], height[right], right - left);
-                if (current > maxVol)
-                {
-                    maxVol = current;
-                }
                 while (left < right && height[left] <= shorter)
                     left++;
-            }
             else
-            {
-                shorter = height[right];
-                current = volume(height[left], height[right], right - left);
-                if (current > maxVol)
-                {
-                    maxVol = current;
-                }
                 while (left < right && height[right] <= shorter)
                     right--;
-            }
         }
         return maxVol;
     }
