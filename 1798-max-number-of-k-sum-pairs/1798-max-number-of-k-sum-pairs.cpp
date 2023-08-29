41014277dd72fde5455a1fdd5c6c4af501d3f406
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2023-08-29 17:03:17 +0530

    Time: 62 ms (99.9%), Space: 60.6 MB (39.89%) - LeetHub

--- 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.cpp
+++ 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int maxOperations(vector<int>& nums, int k) {
+        ios_base::sync_with_stdio(false);
         unordered_map<int,int> umap;
         int n=nums.size();
         int count=0;
