a4a0cd22322fb402ea969e4f13fc73ca61985656
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2023-08-29 17:10:59 +0530

    Time: 62 ms (99.9%), Space: 60.7 MB (39.8%) - LeetHub

--- 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.cpp
+++ 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.cpp
@@ -1,38 +1,38 @@
 class Solution {
 public:
     int maxOperations(vector<int>& nums, int k) {
-        // ios_base::sync_with_stdio(false);
-        // unordered_map<int,int> umap;
-        // int n=nums.size();
-        // int count=0;
-        // for(int i=0;i<n;i++)
-        // {
-        //     if(nums[i]>=k)
-        //         continue;
-        //     if(umap.find(k-nums[i]) == umap.end()||umap[k-nums[i]]==0)
-        //         umap[nums[i]]++;
-        //     else
-        //     {
-        //         umap[k-nums[i]]--;
-        //         count++;
-        //     }
-        // }
-        // return count;
         ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-        cout.tie(NULL);
-        sort(nums.begin(),nums.end());
-        int i{0},j=nums.size()-1;
-        int count{0};
-        while(i<j)
+        unordered_map<int,int> umap;
+        int n=nums.size();
+        int count=0;
+        for(int i=0;i<n;i++)
         {
-            if(nums[i]+nums[j]==k)
-                count++,i++,j--;
-            else if(nums[i]+nums[j]<k)  
-                i++;
-            else  
-                j--;
+            if(nums[i]>=k)
+                continue;
+            if(umap.find(k-nums[i]) == umap.end()||umap[k-nums[i]]==0)
+                umap[nums[i]]++;
+            else
+            {
+                umap[k-nums[i]]--;
+                count++;
+            }
         }
         return count;
+        // ios_base::sync_with_stdio(false);
+        // cin.tie(NULL);
+        // cout.tie(NULL);
+        // sort(nums.begin(),nums.end());
+        // int i{0},j=nums.size()-1;
+        // int count{0};
+        // while(i<j)
+        // {
+        //     if(nums[i]+nums[j]==k)
+        //         count++,i++,j--;
+        //     else if(nums[i]+nums[j]<k)  
+        //         i++;
+        //     else  
+        //         j--;
+        // }
+        // return count;
     }
 };
