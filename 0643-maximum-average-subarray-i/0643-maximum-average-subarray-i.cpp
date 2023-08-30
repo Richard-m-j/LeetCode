b4c82eff201e75c88d469c7fd78c11048b7b6f88
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2023-08-30 21:19:28 +0530

    Time: 102 ms (99.43%), Space: 109.8 MB (8.24%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     double findMaxAverage(vector<int>& nums, int k) {
+        ios_base::sync_with_stdio(false);
         int n=nums.size();
         int max;
         int curr=0;
         for(int i=0;i<k;i++)
-        {
            curr+=nums[i];
-        }
+           
         max=curr;
         for(int i=k;i<n;i++)
         {
