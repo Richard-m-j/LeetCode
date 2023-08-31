b7f398b735eccdfab42fd3cae7a3168e3c2fd15f
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2023-08-31 08:34:48 +0530

    Time: 4 ms (99.89%), Space: 10 MB (59.52%) - LeetHub

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -1,38 +1,31 @@
 class Solution {
 public:
+    bool mp[128];
     int maxVowels(string s, int k) {
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
-        
+        mp['a']=true;
+        mp['e']=true;
+        mp['i']=true;
+        mp['o']=true;
+        mp['u']=true;
         int n=s.length();
         int curr{0};
         for(int i=0;i<k;i++)
-            if(isVowel(s[i]))
+            if(mp[s[i]])
                 curr++;
         int max=curr;
         for(int i=k;i<n;i++)
         {
-            if(isVowel(s[i])&&!isVowel(s[i-k]))
+            if(mp[s[i]]&&!mp[s[i-k]])
             {
                 curr++;
                 if(curr>max)
                     max=curr;           
             }
-            else if(!isVowel(s[i])&&isVowel(s[i-k]))
+            else if(!mp[s[i]]&&mp[s[i-k]])
                 curr--;
         }
         return max;
-    }
-    bool isVowel(char c)
-    {
-        switch(c)
-        {
-            case 'a':
-            case 'e':
-            case 'i':
-            case 'o':
-            case 'u': return true;
-            default :return false;
-        }
     }
 };
