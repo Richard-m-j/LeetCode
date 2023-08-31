19470a4d65f682eabe8b06c050212c7f6a317e9b
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2023-08-31 17:59:42 +0530

    Time: 17 ms (75.78%), Space: 31.3 MB (21.82%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -1,9 +1,13 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
         int n=nums.size();
         int left[n],right[n];
         left[0]=right[n-1]=0;
+
         for(int i=1;i<n;i++)
         {
             left[i]=left[i-1]+nums[i-1];
