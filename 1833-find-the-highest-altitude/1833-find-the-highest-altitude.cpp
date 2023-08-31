ec0e82b5c709798d6d9a3558c4f93b218b3a4343
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2023-08-31 15:29:15 +0530

    Time: 0 ms (100%), Space: 8.2 MB (6.46%) - LeetHub

--- 1833-find-the-highest-altitude/1833-find-the-highest-altitude.cpp
+++ 1833-find-the-highest-altitude/1833-find-the-highest-altitude.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int largestAltitude(vector<int>& gain) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
         int n=gain.size();
         int curr{0},highest{0};
         for(int i=0;i<n;i++)
