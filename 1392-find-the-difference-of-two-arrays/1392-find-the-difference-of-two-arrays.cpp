23eac3706e748cfbb89e474ce6d43f48989e34a6
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2023-09-01 11:52:46 +0530

    Time: 12 ms (99.92%), Space: 26.9 MB (92.1%) - LeetHub

--- 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.cpp
+++ 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.cpp
@@ -1,18 +1,32 @@
 class Solution {
 public:
+    bool inNums1[2001];
+    bool inNums2[2001];
     vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
         cout.tie(NULL);
         vector <int> list1,list2;
-        sort(nums1.begin(), nums1.end());
-        nums1.erase(unique(nums1.begin(), nums1.end()), nums1.end());
 
-        sort(nums2.begin(), nums2.end());
-        nums2.erase(unique(nums2.begin(), nums2.end()), nums2.end());
+        for(auto& i: nums1)
+            inNums1[1000+i]=true;
+        
+        for(auto& i: nums2)
+            if(!inNums2[1000+i]&&!inNums1[1000+i])
+            {
+                inNums2[1000+i]=true;
+                list2.push_back(i);
+            }
+            else if(!inNums2[1000+i])
+                inNums2[1000+i]=true;
 
-        set_difference(nums1.begin(),nums1.end(),nums2.begin(),nums2.end(),back_inserter(list1));
-        set_difference(nums2.begin(),nums2.end(),nums1.begin(),nums1.end(),back_inserter(list2));
+        for(auto& i: nums1)
+            if(!inNums2[1000+i]&&inNums1[1000+i])
+            {
+                inNums1[1000+i]=false;
+                list1.push_back(i);
+            }
+        
     return {list1,list2};
     }
 };
