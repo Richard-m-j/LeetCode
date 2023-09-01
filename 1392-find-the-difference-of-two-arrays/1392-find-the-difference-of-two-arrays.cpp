fbc5f8c735549bc440a9221089b4dd089ee2307a
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2023-09-01 11:01:58 +0530

    Time: 27 ms (97.24%), Space: 27 MB (92.1%) - LeetHub

--- 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.cpp
+++ 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
         vector <int> list1,list2;
         sort(nums1.begin(), nums1.end());
         nums1.erase(unique(nums1.begin(), nums1.end()), nums1.end());
