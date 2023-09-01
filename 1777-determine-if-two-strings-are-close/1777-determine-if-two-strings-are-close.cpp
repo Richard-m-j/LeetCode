cfa7eb5578cb9b5c5a5db1cee769ad13942c29aa
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2023-09-01 22:07:49 +0530

    Time: 26 ms (99.49%), Space: 20.5 MB (72.68%) - LeetHub

--- 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.cpp
+++ 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
-    int f1[26],f2[26];
     bool closeStrings(string word1, string word2) {
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
         cout.tie(NULL);
-        if(word1.length()!=word2.length())
-            return false;
-        
-        for(auto& i: word1)
-            f1[i-'a']++;
-
-        for(auto& i: word2)
-            f2[i-'a']++;
-
-        for(int i=0;i<26;i++)
-            if(f1[i]>0&&!f2[i]||f2[i]>0&&!f1[i])
+        int vec1[26]={};
+        int vec2[26]={};
+        for(char i:word1){
+            vec1[i-'a']++;
+        }
+        for(char i:word2){
+            vec2[i-'a']++;
+        }
+        for(int i=0;i<26;i++){
+            if(vec1[i]>0 && vec2[i]==0 || (vec2[i]>0 && vec1[i]==0)){
                 return false;
-        
-        sort(f1,f1+26);
-        sort(f2,f2+26);
-
-        for(int i=0;i<26;i++)
-            if(f1[i]!=f2[i])
+            }
+        }
+        sort(vec1,vec1+26);
+        sort(vec2,vec2+26);
+        for(int i=0;i<26;i++){
+            if(vec1[i]!=vec2[i]){
                 return false;
-
+            }
+        }
         return true;
     }
 };
