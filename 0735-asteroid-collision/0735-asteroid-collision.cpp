f0f1f905cd1bf37ebf37781ba247c137066aa9ff
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2023-09-02 21:29:23 +0530

    Time: 4 ms (98.99%), Space: 17.4 MB (81.03%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -0,0 +1,38 @@
+#include <vector>
+
+class Solution {
+public:
+    std::vector<int> asteroidCollision(std::vector<int>& asteroids) {
+        std::vector<int> stack;
+
+        for (int asteroid : asteroids) {
+            bool collisionHandled = false;
+            
+            while (!stack.empty() && stack.back() > 0 && asteroid < 0) {
+                int top = stack.back();
+                stack.pop_back();
+                
+                if (-asteroid > top) {
+            // Current asteroid survives, continue checking with the next stack element.
+                    continue;
+                } else if (-asteroid == top) {
+                    // Both asteroids annihilate each other.
+                    collisionHandled = true;
+                    break;
+                } 
+                else {
+                    // Top asteroid survives, push it back, and continue checking.
+                    stack.push_back(top);
+                    collisionHandled = true;
+                    break;
+                }
+            }
+            
+            if (!collisionHandled) {
+                stack.push_back(asteroid);
+            }
+        }
+
+        return stack;
+    }
+};
