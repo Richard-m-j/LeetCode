10d029dbb2218545445b11bbe2b1f41d06c1ef8b
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2023-09-04 21:07:16 +0530

    Time: 28 ms (100%), Space: 24.3 MB (64.36%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -4,11 +4,13 @@ public:
         vector<vector<int>> ans;
         if (nums.size() < 3) 
             return ans;
-        int min =  200000, max = -200000;
+        int min =  INT_MAX, max = INT_MIN;
         for (int i = 0; i < nums.size(); i++) 
         {
-            if (min > nums[i]) min = nums[i];
-            if (max < nums[i]) max = nums[i];    
+            if (min > nums[i]) 
+                min = nums[i];
+            if (max < nums[i]) 
+                max = nums[i];    
         }
         const int range = -min, size = max-min+1;
         char store[size]; 
@@ -18,28 +20,27 @@ public:
             if (store[nums[i]+range] < 3) 
                 store[nums[i]+range]++; 
         int len = 0;
-        for (int i = 0, j = 0; i < size; i++) {
-            if (store[i] > 0) {
-                nums[j++] = i-range;
-                len++;
-            }
+        for (int i = 0; i < size; i++) 
+        {
+            if (store[i] > 0) 
+                nums[len++] = i-range;
+            
         }
-        vector<int>& unique = nums;
 
-        if (unique[0] > 0 || unique[len-1] < 0) 
+        if (nums[0] > 0 || nums[len-1] < 0) 
             return ans;
         else if (len == 1) {
-            if (unique[0]==0 && store[range]>2) 
+            if (nums[0]==0 && store[range]>2) 
                 ans.push_back({0,0,0});
             return ans;
         }
-        const int high = unique[len-1], zero = store[range];
+        const int high = nums[len-1], zero = store[range];
         for (int i = 0; i < len; i++) {
-            int a = unique[i];
+            int a = nums[i];
             if (a > 0) break;
             int j = (store[a+range] > 1) ? i : i+1;
             for (; j < len; j++) {
-                int b = unique[j];
+                int b = nums[j];
                 int c = -a-b;
                 if (b > -a/2) break;
                 else if (c > high || c < b) continue;
