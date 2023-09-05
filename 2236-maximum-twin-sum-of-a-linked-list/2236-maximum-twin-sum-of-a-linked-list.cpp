52db64600a970c7ac2fce66e51f03eb2da5934c5
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2023-09-05 13:34:34 +0530

    Time: 149 ms (99.46%), Space: 126.5 MB (33.28%) - LeetHub

--- 2236-maximum-twin-sum-of-a-linked-list/2236-maximum-twin-sum-of-a-linked-list.cpp
+++ 2236-maximum-twin-sum-of-a-linked-list/2236-maximum-twin-sum-of-a-linked-list.cpp
@@ -1,19 +1,11 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+#pragma GCC optimize("fast")
+static auto _ = [] () {ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);return 0;}();
+
+
 class Solution {
 public:
     int pairSum(ListNode* head) {
-        ios_base::sync_with_stdio(false);
-        cin.tie(nullptr);
-        cout.tie(nullptr);
+        
         int max=INT_MIN;
         vector <int> dq;
         ListNode *ptr=head;
