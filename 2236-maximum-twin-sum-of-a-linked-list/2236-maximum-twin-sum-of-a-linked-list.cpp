dc9999980b33438afbf9391749776647191e8c60
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2023-09-05 13:35:10 +0530

    Time: 138 ms (99.72%), Space: 126.4 MB (33.28%) - LeetHub

--- 2236-maximum-twin-sum-of-a-linked-list/2236-maximum-twin-sum-of-a-linked-list.cpp
+++ 2236-maximum-twin-sum-of-a-linked-list/2236-maximum-twin-sum-of-a-linked-list.cpp
@@ -1,27 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
 #pragma GCC optimize("fast")
 static auto _ = [] () {ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);return 0;}();
-
 
 class Solution {
 public:
     int pairSum(ListNode* head) {
-        
-        int max=INT_MIN;
-        vector <int> dq;
-        ListNode *ptr=head;
-        int n{0};
-        while(ptr)
-        {
-            dq.push_back(ptr->val);
-            ptr=ptr->next;
-            n++;
-        }
-        for(int i=0;i<n/2;i++)
-        {
-            int sum = dq[i]+dq[n-i-1];
-            if(sum>max)
-                max=sum;
-        }
-        return max;
+        vector<int> values; values.reserve(1000);
+        for (ListNode* itr = head; itr; itr = itr->next) values.push_back(itr->val);
+        if (values.size() > 100000) return values.size();
+        int res = 0, n = values.size();
+        for (int i = 0; i < n / 2; ++i) res = max(values[i] + values[n-1-i], res);
+        return res;
     }
 };
