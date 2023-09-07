c2f94a8184c669569db047be89ecc7d7ed48a02b
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2023-09-07 19:07:26 +0530

    Time: 7 ms (98.48%), Space: 17.1 MB (22.23%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -3,7 +3,7 @@ public:
     int singleNumber(vector<int>& nums) {
         ios_base::sync_with_stdio(false);
         cin.tie(nullptr);
-        cout.tie(nullptr);
+
         int ans = 0;
         for (int num : nums)
             ans = ans ^ num;
