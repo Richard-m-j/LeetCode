4bad35ba11b59a451a485fa5f91ec307c576715a
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2023-09-07 14:17:17 +0530

    Time: 50 ms (91.6%), Space: 17.9 MB (19.59%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.py
+++ 0437-path-sum-iii/0437-path-sum-iii.py
@@ -1,30 +1,20 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
-        def dfs(node, current_sum, path_sum):
+        def dfs(node, pathSum):
             if not node:
-                return 0
-            
-            current_sum += node.val
-            path_sum += current_sum == targetSum
-            
-            return (
-                path_sum
-                + dfs(node.left, current_sum, 0)
-                + dfs(node.right, current_sum, 0)
-            )
-        
-        if not root:
-            return 0
-        
-        return (
-            dfs(root, 0, 0)
-            + self.pathSum(root.left, targetSum)
-            + self.pathSum(root.right, targetSum)
-        )
+                return 
+            currentSum = pathSum + node.val
+            x = currentSum - targetSum
+            if x in freq:
+                self.count += freq[x]
+            if currentSum in freq:
+                freq[currentSum] += 1
+            else:
+                freq[currentSum]=1
+            dfs(node.left,currentSum)
+            dfs(node.right,currentSum)
+            freq[currentSum] -= 1
+        self.count = 0
+        freq = {0:1}
+        dfs(root,0)
+        return self.count
