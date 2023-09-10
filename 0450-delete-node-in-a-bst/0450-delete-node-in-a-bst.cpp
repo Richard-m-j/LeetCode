58564515731bc2ecff8236ee0e434413f8c5f8a2
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2023-09-10 10:42:46 +0530

    Time: 27 ms (72.98%), Space: 32.6 MB (60.13%) - LeetHub

---
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -0,0 +1,98 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if(!root)
+            return root;
+        if(root->val == key)
+        {
+            TreeNode* succ = root->right;
+            TreeNode* ptr = root;
+        
+            while(succ && succ->left)
+            {
+                ptr = succ;
+                succ= succ->left;
+            }
+            
+            if(succ)
+            {
+                root->val = succ->val;
+                search(succ,ptr,succ->val);                
+            }
+            else
+            {
+                root = root->left;
+            }
+        }
+        else if(key < root->val)
+            search(root->left,root,key);
+        else
+            search(root->right,root,key);
+        return root;       
+        
+    }
+    TreeNode* search(TreeNode* node,TreeNode* parent, int key)
+    {
+        if(!node)
+            return NULL;
+        if(key < node->val)
+            return search(node->left,node,key);
+        if(key > node->val)
+            return search(node->right,node,key);
+        
+        if(node->left && node->right)
+        {
+            TreeNode* succ = node->right;
+            TreeNode* ptr= node;
+        
+            while(succ && succ->left)
+            {
+                ptr = succ;
+                succ= succ->left;
+            }
+            node->val = succ->val;
+            search(succ,ptr,succ->val);
+        }
+        else if(!node->left)
+        {
+            if(parent->left == node)
+                parent->left = node->right;
+            else
+                parent->right = node->right;
+        }
+        else
+        {
+            if(parent->left == node)
+                parent->left = node->left;
+            else
+                parent->right = node->left;
+        }
+        return node;
+    }
+    
+    TreeNode* inorderSuccessor(TreeNode* node)
+    {
+        // if(!node || !node->left && !node->right)
+        //     return NULL;
+        // if(!node->left)
+        //     return node->right;
+        // if(!node->right)
+        //     return node->left;
+        TreeNode* ptr=node->right;
+        
+        while(ptr && ptr->left)
+            ptr= ptr->left;
+        return ptr;
+    }
+};
