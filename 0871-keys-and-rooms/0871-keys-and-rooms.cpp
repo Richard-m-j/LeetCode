a73af8515144c27676926f2f96c541b711591ba2
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2023-09-11 10:04:59 +0530

    Time: 26 ms (5.45%), Space: 10.6 MB (33.11%) - LeetHub

---
+++ 0871-keys-and-rooms/0871-keys-and-rooms.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        vector<vector<int>> roomsFound;
+        roomsFound.push_back(rooms[0]);
+        vector<bool> keysFound(rooms.size(), false);
+        keysFound[0] = true;
+
+        while (!roomsFound.empty()) {
+            vector<int> room = roomsFound[0];
+            roomsFound.erase(roomsFound.begin()); // Remove the first room from the list
+
+            for (auto key : room) {
+                if (!keysFound[key]) {
+                    roomsFound.push_back(rooms[key]);
+                    keysFound[key] = true;
+                }
+            }
+        }
+
+        for (bool i : keysFound) {
+            if (!i) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
