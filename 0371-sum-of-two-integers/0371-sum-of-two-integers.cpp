704134ff810b942f2645379dba0592c21a04f2ae
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2023-09-12 14:18:27 +0530

    Time: 2 ms (53.64%), Space: 6.1 MB (15.19%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
@@ -1,15 +1,6 @@
 class Solution {
 public:
     int getSum(int a, int b) {
-        ios_base::sync_with_stdio(false);
-        cin.tie(nullptr);
-        cout.tie(nullptr);
-        while(b)
-        {
-            int c = a & b;
-            a = a ^ b;
-            b = c << 1;
-        }
-        return a;
+        return (a ^ b) + ((a & b) << 1);
     }
 };
