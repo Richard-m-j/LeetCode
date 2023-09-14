fcac16e4cc6aba6d8b7b7b8d87eeffb8d1dd8e6e
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2023-09-14 14:41:13 +0530

    Time: 270 ms (75.68%), Space: 99 MB (96.28%) - LeetHub

---
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int minReorder(int n, vector<vector<int>>& connections) {
+        vector <vector <pair <int, bool>>> graph(n);
+
+        // Build the graph with direction information
+        for (const auto& con : connections) {
+            graph[con[0]].emplace_back(con[1], true);   // Forward edge
+            graph[con[1]].emplace_back(con[0], false);  // Reverse edge
+        }
+
+        int ans = 0;
+        vector<bool> visited(n, false);
+        stack<int> stack;
+        stack.push(0);
+
+        while (!stack.empty()) 
+        {
+            int node = stack.top();
+            stack.pop();
+            visited[node] = true;
+
+            for (const auto& neighbor : graph[node]) 
+            {
+                int nextNode = neighbor.first;
+                bool isForwardEdge = neighbor.second;
+
+                if (!visited[nextNode]) 
+                {
+                    if (isForwardEdge) 
+                        ans++;  // Reorder needed for this edge
+                    
+                    stack.push(nextNode);
+                }
+            }
+        }
+
+        return ans;
+    }
+};
