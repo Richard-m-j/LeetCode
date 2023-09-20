0aa9bcef4ca7f4de0353a0dc443cd72fda9a603c
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2023-09-20 21:15:41 +0530

    Time: 23 ms (70.22%), Space: 71.7 MB (6.81%) - LeetHub

---
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -0,0 +1,77 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        int carry{0};
+        ListNode* head;
+        ListNode* ptr;
+        head = new ListNode();
+        ptr = head;
+        while(l1 && l2)
+        {
+            int sum = l1->val + l2->val + carry;
+            if(sum>9)
+            {
+                carry = sum / 10;
+                sum %= 10;
+            }
+            else
+                carry = 0;
+            ptr->next = new ListNode(sum);
+            ptr = ptr->next;
+            l1 = l1->next;
+            l2 = l2->next;
+        }
+        while(l1)
+        {
+            int sum = l1->val + carry;
+            if(sum>9)
+            {
+                carry = sum / 10;
+                sum %= 10;
+            }
+            else
+                carry = 0;
+            ptr->next = new ListNode(sum);
+            ptr = ptr->next;
+            l1 = l1->next;
+        }
+        while(l2)
+        {
+            int sum =  l2->val + carry;
+            if(sum>9)
+            {
+                carry = sum / 10;
+                sum %= 10;
+            }
+            else
+                carry = 0;
+            ptr->next = new ListNode(sum);
+            ptr = ptr->next;
+            l2 = l2->next;
+        }
+        while(carry)
+        {
+            int sum =  carry;
+            if(sum>9)
+            {
+                carry = sum / 10;
+                sum %= 10;
+            }
+            else
+                carry = 0;
+            ptr->next = new ListNode(sum);
+            ptr = ptr->next;
+        }
+        return head->next;
+    }
+};
