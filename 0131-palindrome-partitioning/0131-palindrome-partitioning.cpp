d4609db76ea27084457cf099e618fc143a2a7d8b
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2023-10-16 16:39:40 +0530

    Time: 100 ms (61.24%), Space: 58.1 MB (39.44%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     vector<vector<string>> partition(string s) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+        
         vector<vector<string>> res;
         vector<string> path;
         n = s.length();
