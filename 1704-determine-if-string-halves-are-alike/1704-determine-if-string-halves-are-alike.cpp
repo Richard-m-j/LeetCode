6537b062c188bcce2fd27c55c31abbbde39d1b39
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-01-12 12:44:04 +0530

    Time: 3 ms (52.79%), Space: 7.3 MB (5.15%) - LeetHub

---
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
@@ -0,0 +1,23 @@
+class Solution {
+private:
+    unordered_map <char,bool> vowels = {{'A',true},{'E',true},{'I',true},{'O',true},{'U',true},{'a',true},{'e',true},{'i',true},{'o',true},{'u',true}};
+
+public:
+    bool halvesAreAlike(string s) {
+        int l = 0, r= s.length()-1;
+        int numl = 0;
+        int numr = 0;
+        while(l<r)
+        {
+            if(vowels[s[l]])
+                numl++;
+            if(vowels[s[r]])
+                numr++;
+            l++;
+            r--;
+        }
+        if(numl == numr)
+            return true;
+        return false;
+    }
+};
