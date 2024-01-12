b666f2785373d65bfd2d8c57686525fc630fd0b6
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-01-12 11:29:08 +0530

    Time: 22 ms (67.73%), Space: 71.7 MB (37.64%) - LeetHub

---
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.cpp
@@ -0,0 +1,52 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+**/
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        string num1 = "", num2 = "";
+
+        // Convert linked list values to strings
+        ListNode* ptr = l1;
+        while (ptr) {
+            num1 += to_string(ptr->val);
+            ptr = ptr->next;
+        }
+
+        ptr = l2;
+        while (ptr) {
+            num2 += to_string(ptr->val);
+            ptr = ptr->next;
+        }
+
+        int n1 = num1.length();
+        int n2 = num2.length();
+        ListNode* ans = new ListNode();
+        ListNode* ptrAns = ans; // Use a separate pointer for the result list
+        int carry = 0;
+        int i = n1 - 1, j = n2 - 1;
+
+        while (i >= 0 || j >= 0 || carry) {
+            int digit1 = (i >= 0) ? num1[i--] - '0' : 0;
+            int digit2 = (j >= 0) ? num2[j--] - '0' : 0;
+
+            int sum = digit1 + digit2 + carry;
+
+            carry = sum / 10;
+            sum = sum % 10;
+            ptrAns -> val = sum;
+            ans = new ListNode();
+            ans -> next = ptrAns; 
+            ptrAns = ans;
+        }
+
+        return ans->next; // Skip the dummy node
+    }
+};
