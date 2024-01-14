67d5a971cf67b064a610afafe440597342d40f2c
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-01-14 10:07:19 +0530

    Time: 23 ms (100%), Space: 20.9 MB (78.4%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
@@ -1,30 +1,27 @@
+int speedUp = []{ios::sync_with_stdio(0);cin.tie(0);return 0;}();
 class Solution {
 public:
-    
     bool closeStrings(string word1, string word2) {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-        cout.tie(NULL);
-        int f1[26]={};
-        int f2[26]={};
-        if(word1.length()!=word2.length())
+        int f1[26] = {};
+        int f2[26] = {};
+        if (word1.length() != word2.length())
             return false;
-        
-        for(char i: word1)
-            f1[i-'a']++;
 
-        for(char i: word2)
-            f2[i-'a']++;
+        for (char i : word1)
+            f1[i - 'a']++;
 
-        for(int i=0;i<26;i++)
-            if(f1[i]>0&&!f2[i]||f2[i]>0&&!f1[i])
+        for (char i : word2)
+            f2[i - 'a']++;
+
+        for (int i = 0; i < 26; i++)
+            if (f1[i] > 0 && !f2[i] || f2[i] > 0 && !f1[i])
                 return false;
-        
-        sort(f1,f1+26);
-        sort(f2,f2+26);
 
-        for(int i=0;i<26;i++)
-            if(f1[i]!=f2[i])
+        sort(f1, f1 + 26);
+        sort(f2, f2 + 26);
+
+        for (int i = 0; i < 26; i++)
+            if (f1[i] != f2[i])
                 return false;
 
         return true;
