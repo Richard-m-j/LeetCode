228683bbc881480b5de83b723e7a4b168026e9e4
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-01-21 09:00:46 +0530

    Time: 0 ms (100%), Space: 9.4 MB (9.67%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,25 @@
+class Solution {
+private:
+    vector<int> houses;
+    vector<int> maxMoney;
+public:
+    int rob(vector<int>& nums) {
+        houses = nums;
+        maxMoney.resize(nums.size(),0);
+        int i = 0;
+        while(i<houses.size() && houses[i]==0)
+            i++;
+        int res = max(helper(i),helper(i+1));
+        return res;
+    }
+    int helper(int i)
+    {
+        if(i > houses.size() - 1)
+            return 0;
+        if(maxMoney[i] != 0)
+            return maxMoney[i];
+        
+        maxMoney[i] = houses[i] + max(helper(i+2),helper(i+3));
+        return maxMoney[i];
+    }
+};
