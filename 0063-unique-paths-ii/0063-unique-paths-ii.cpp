a2ab00a82328d102028ca644af355fe6e405e079
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-01-26 22:02:11 +0530

    Time: 7 ms (8.66%), Space: 10.3 MB (6.9%) - LeetHub

---
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+private:
+    vector<vector<int>> grid;
+    vector<vector<int>> dp;
+    int m,n;
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        grid = obstacleGrid;
+        m = grid.size();
+        n = grid[0].size();
+        dp.resize(m,vector<int>(n,-1));
+        dp[m-1][n-1] = 1;
+        return helper(0,0);
+    }
+    int helper(int row, int col)
+    {
+        if(row == m || row < 0 || col == n || col < 0 || grid[row][col] == 1)
+            return 0;
+        if(dp[row][col] != -1)
+            return dp[row][col];
+        dp[row][col] = helper(row+1, col) + helper(row,col+1);
+        return dp[row][col];
+    }
+};
