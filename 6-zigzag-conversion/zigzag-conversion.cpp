dab4d941e1ab9603c64141c16b672e8c35bd8bab
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-01-31 22:41:58 +0530

    Time: 3 ms (98.52%) | Memory: 12.9 MB (34.16%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.cpp
+++ 6-zigzag-conversion/zigzag-conversion.cpp
@@ -1,25 +1,31 @@
-static const int speedUp = []{ios::sync_with_stdio(0);cin.tie(0);return 0;}();
+static const int speedUp = [] {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    return 0;
+}();
 class Solution {
 public:
     string convert(string s, int numRows) {
-        if (numRows == 1 || s.length() <= numRows) return s;
-        
+        if (numRows == 1 || s.length() <= numRows)
+            return s;
+
         vector<string> rows(numRows);
         int rowIndex = 0;
         bool goingDown = false;
-        
-        for (char &c : s) {
+
+        for (char& c : s) {
             rows[rowIndex] += c;
-            
-            if (rowIndex == 0 || rowIndex == numRows - 1) goingDown = !goingDown;
+
+            if (rowIndex == 0 || rowIndex == numRows - 1)
+                goingDown = !goingDown;
             rowIndex += goingDown ? 1 : -1;
         }
-        
-        string result;
+
+        string result ="";
         for (string& row : rows) {
             result += row;
         }
-        
+
         return result;
     }
 };
