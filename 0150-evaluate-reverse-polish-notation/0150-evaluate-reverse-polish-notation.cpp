01d78a05da657312f8b5774aaca15067faa860bb
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-01-31 06:08:07 +0530

    Time: 4 ms (93.92%), Space: 15.5 MB (11.31%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -2,34 +2,33 @@ static const int speedUp = []{ios::sync_with_stdio(0);cin.tie(0);return 0;}();
 class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
-        stack <int> tokenStack;
-        int i = 1;
-        tokenStack.push(stoi(tokens[0]));
-        for(int i =1;i<tokens.size(); i++)
-        {
-            string current = tokens[i];
-            if(current == "+" || current == "-" || current == "*" || current == "/")
-            {
-                if(tokenStack.size() < 2)
+        stack<int> tokenStack;
+        for (const string& token : tokens) {
+            if (isOperator(token)) {
+                if (tokenStack.size() < 2)
                     return -1;
-                int a = tokenStack.top();
-                tokenStack.pop();
-                int b = tokenStack.top();
-                tokenStack.pop();
-                if(current == "+")
-                    tokenStack.push(a+b);
-                else if(current == "-")
-                    tokenStack.push(b-a);
-                else if(current == "*")
-                    tokenStack.push(a*b);
-                else 
-                    tokenStack.push(b/a);
+                int a = tokenStack.top(); tokenStack.pop();
+                int b = tokenStack.top(); tokenStack.pop();
+                tokenStack.push(applyOperator(token, b, a));
+            } else {
+                tokenStack.push(stoi(token));
             }
-            else
-                tokenStack.push(stoi(current));
         }
-        if(tokenStack.size() == 1)
-            return tokenStack.top();
-        return -1;
+        return (tokenStack.size() == 1) ? tokenStack.top() : -1;
+    }
+
+private:
+    bool isOperator(const string& token) {
+        return token == "+" || token == "-" || token == "*" || token == "/";
+    }
+
+    int applyOperator(const string& op, int a, int b) {
+        switch (op[0]) {
+            case '+': return a + b;
+            case '-': return a - b;
+            case '*': return a * b;
+            case '/': return a / b;
+            default: return 0; 
+        }
     }
 };
