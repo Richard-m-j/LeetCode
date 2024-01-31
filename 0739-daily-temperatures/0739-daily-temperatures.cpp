1714fb935111e4dad91e3bd0b1f8d6134de8f2fa
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-01-31 11:28:04 +0530

    Time: 131 ms (77.16%), Space: 101.1 MB (64.78%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,16 +1,17 @@
+static 
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
         stack<int> indexStack;
         int n = temperatures.size();
         vector<int> res(n,0);
-        for (int i = n-1; i >= 0; i--) {
+        for (int i = n-1; i >= 0; i--) 
+        {
             while(!indexStack.empty() && temperatures[indexStack.top()] <= temperatures[i])
                 indexStack.pop();
             if(!indexStack.empty())
-            {
                 res[i] = indexStack.top() - i;
-            }
+            
             indexStack.push(i);
         }
         return res;
