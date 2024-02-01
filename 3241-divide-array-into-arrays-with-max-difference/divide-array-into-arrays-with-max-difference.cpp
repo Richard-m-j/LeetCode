46bb800911342cdfd42a68fbab7d406f8e3ad1ba
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-02-01 06:59:34 +0530

    Time: 113 ms (99.91%) | Memory: 119.1 MB (39.70%) - LeetSync

--- 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.cpp
+++ 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.cpp
@@ -1,3 +1,8 @@
+static const int ZERO = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    return 0;
+}();
 class Solution {
 public:
     vector<vector<int>> divideArray(vector<int>& nums, int k) {
