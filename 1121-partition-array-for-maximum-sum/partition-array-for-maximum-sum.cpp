1a18a4b4d0fb608da22460768575beea529a48d9
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-02-03 09:55:14 +0530

    Time: 8 ms (87.76%) | Memory: 10.7 MB (26.98%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
@@ -5,19 +5,18 @@ public:
         cin.tie(nullptr);
         cout.tie(nullptr);
         int n = arr.size();
-        int dp[n + 1]; // dp[i] represents the maximum sum possible up to index i
+        int dp[n + 1]; 
 
         dp[0] = 0;
 
         for (int i = 1; i <= n; ++i) {
             int maxElement = arr[i - 1];
-            dp[i] = dp[i - 1] + arr[i - 1]; // Initialize with single element partition sum
+            dp[i] = dp[i - 1] + arr[i - 1]; 
             for (int j = 2; j <= k && i - j >= 0; ++j) {
-                maxElement = max(maxElement, arr[i - j]); // Update maxElement for current partition
-                dp[i] = max(dp[i], dp[i - j] + maxElement * j); // Update dp[i] considering partition size j
+                maxElement = max(maxElement, arr[i - j]); 
+                dp[i] = max(dp[i], dp[i - j] + maxElement * j); 
             }
         }
-
         return dp[n];
     }
 };
