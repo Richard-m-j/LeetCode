1d7bfcd58b64705f99d0145d27b40b9d70b8ec40
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-02-03 11:15:02 +0530

    Time: 60 ms (66.19%) | Memory: 29.6 MB (34.34%) - LeetSync

---
+++ 436-find-right-interval/find-right-interval.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> findRightInterval(vector<vector<int>>& intervals) {
+        int n = intervals.size();
+        vector<int> res(n, -1);
+        for(int i =0;i<n;i++)
+            intervals[i].push_back(i);
+        sort(intervals.begin(), intervals.end(), compareStart);
+
+        for (int i = 0; i < n; ++i) {
+            int target = intervals[i][1];
+            int l = i, r = n - 1, result = -1;
+            while (l <= r) {
+                int m = l + (r - l) / 2;
+                if (intervals[m][0] >= target) {
+                    result = intervals[m][2];
+                    r = m - 1;
+                } else {
+                    l = m + 1;
+                }
+            }
+            res[intervals[i][2]] = result;
+        }
+        
+        return res;
+    }
+    
+    static bool compareStart(const vector<int>& a, const vector<int>& b) {
+        return a[0] < b[0];
+    }
+};
