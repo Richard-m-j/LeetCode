25d2a6a90d9d5221852252b8dff7de16f39edeaa
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-02-08 08:57:27 +0530

    Time: 858 ms (5.01%) | Memory: 10.6 MB (32.42%) - LeetSync

--- 279-perfect-squares/perfect-squares.cpp
+++ 279-perfect-squares/perfect-squares.cpp
@@ -6,6 +6,8 @@ public:
         vector<int> dp(n + 1, INT_MAX);
         for (int i = 1; i * i <= n; i++)
             dp[i * i] = 1;
+        if(dp[n] == 1)
+            return 1;
         for (int i = 2; i <= n; i++) {
             if (dp[i] == 1)
                 continue;
