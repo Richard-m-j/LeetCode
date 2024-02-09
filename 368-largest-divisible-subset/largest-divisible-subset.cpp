6cc9f9730720bd238ddeecf8e9a481ad2701658c
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-02-09 07:34:13 +0530

    Time: 27 ms (63.90%) | Memory: 13.4 MB (6.28%) - LeetSync

--- 368-largest-divisible-subset/largest-divisible-subset.cpp
+++ 368-largest-divisible-subset/largest-divisible-subset.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     vector<int> largestDivisibleSubset(vector<int>& nums) {
+        ios::sync_with_stdio(0);
+        cin.tie(0);
         int n = nums.size();
         vector<vector<int>> dp(n, vector<int>());
         sort(nums.begin(), nums.end());
         dp[0] = {nums[0]};
         int maxIndex = 0;
         for (int i = 1; i < n; i++) {
-            for (int j = i-1; j >= 0; j--) {
-                if (nums[i] % nums[j] == 0) {
-                    if(dp[i].size()<dp[j].size()+1){
-                        dp[i] = dp[j];
-                    }
-                }
-            }
+            for (int j = i - 1; j >= 0; j--)
+                if (nums[i] % nums[j] == 0 && dp[i].size() < dp[j].size() + 1)
+                    dp[i] = dp[j];
+
             dp[i].push_back(nums[i]);
-            maxIndex = dp[i].size() > dp[maxIndex].size()? i: maxIndex;
+            maxIndex = dp[i].size() > dp[maxIndex].size() ? i : maxIndex;
         }
         return dp[maxIndex];
     }
