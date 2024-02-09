1615baea15044a8ebf27058efe95e0f1e720466c
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-02-09 08:10:00 +0530

    Time: 16 ms (78.83%) | Memory: 11.3 MB (31.82%) - LeetSync

--- 368-largest-divisible-subset/largest-divisible-subset.cpp
+++ 368-largest-divisible-subset/largest-divisible-subset.cpp
@@ -4,18 +4,34 @@ public:
         ios::sync_with_stdio(0);
         cin.tie(0);
         int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>());
+        if (n == 0) return {}; // Handle empty input
+        
         sort(nums.begin(), nums.end());
-        dp[0] = {nums[0]};
-        int maxIndex = 0;
+        
+        vector<int> dp(n, 1); // Stores the length of the largest divisible subset ending at index i
+        vector<int> prevList(n, -1); // Stores the previous index
+        
+        int maxLen = 1, maxIndex = 0;
+        
         for (int i = 1; i < n; i++) {
-            for (int j = i - 1; j >= 0; j--)
-                if (nums[i] % nums[j] == 0 && dp[i].size() < dp[j].size() + 1)
-                    dp[i] = dp[j];
-
-            dp[i].push_back(nums[i]);
-            maxIndex = dp[i].size() > dp[maxIndex].size() ? i : maxIndex;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] % nums[j] == 0 && dp[i] < dp[j] + 1) {
+                    dp[i] = dp[j] + 1;
+                    prevList[i] = j;
+                    if (dp[i] > maxLen) {
+                        maxLen = dp[i];
+                        maxIndex = i;
+                    }
+                }
+            }
         }
-        return dp[maxIndex];
+        
+        vector<int> res;
+        while (maxIndex != -1) {
+            res.push_back(nums[maxIndex]);
+            maxIndex = prevList[maxIndex];
+        }
+        
+        return res;
     }
 };
