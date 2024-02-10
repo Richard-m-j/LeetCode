b77f297df6ff1eb62c0e2f89e4c8d3e706d6ccea
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-02-10 10:17:36 +0530

    Time: 689 ms (5.59%) | Memory: 648.1 MB (5.20%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -2,20 +2,27 @@ class Solution {
 public:
     int countSubstrings(string s) {
         int n = s.length();
-        vector<int> dp(n + 1, 0);
+        unordered_map<string,bool> dp;
         int count = 0;
+
+        // Single characters are palindromes
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (s[j] == s[i]) {
-                    string sub = s.substr(j, i - j + 1);
-                    string rev = sub;
-                    reverse(rev.begin(), rev.end());
-                    if (rev == sub)
-                        count++;
-                }
-            }
+            dp[s.substr(i, 1)] = true;
             count++;
         }
+
+        // Check palindromic substrings of length 2 or more
+        for (int len = 2; len <= n; len++) {
+            for (int start = 0; start <= n - len; start++) {
+                int end = start + len - 1;
+                string sub = s.substr(start, len);
+                if ((s[start] == s[end]) && (len == 2 || dp[sub.substr(1, len - 2)])) {
+                    dp[sub] = true;
+                    count++;
+                }
+            }
+        }
+
         return count;
     }
 };
