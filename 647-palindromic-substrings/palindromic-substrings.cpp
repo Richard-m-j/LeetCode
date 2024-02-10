5e32363510131da1575d3bc14ef651a89083315a
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-02-10 10:40:44 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (69.49%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    int palin(int i, int j,string &s){
-        int cnt = 0;
-        while(i >= 0 && j < s.length() && s[i] == s[j]){
-            cnt++;
-            i--;
-            j++;
-        }
-        return cnt;
-    }
     int countSubstrings(string s) {
         int cnt = 0;
+        auto palin = [](int i, int j,string &s){
+            int count = 0;
+            while(i >= 0 && j < s.length() && s[i] == s[j]){
+                count++;
+                i--;
+                j++;
+            }
+            return count;
+        };
         for(int i = 0; i < s.length() ;i++){
            int j = i;
            int oddLengthVale = palin(i,j , s);
