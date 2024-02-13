13b1589b861a0310f0d5192b37b3b58306b95454
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-02-13 06:54:58 +0530

    Time: 11 ms (100.00%) | Memory: 23.3 MB (61.43%) - LeetSync

--- 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.cpp
+++ 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     string firstPalindrome(vector<string>& words) {
-        for(string& word: words){
-            int l=0, r=word.length()-1;
-            for(;l<=r;l++,r--)
-                if(word[l] != word[r])
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        for (string& word : words) {
+            int l = 0, r = word.length() - 1;
+            for (; l <= r; l++, r--)
+                if (word[l] != word[r])
                     break;
-            if(l>r)
+            if (l > r)
                 return word;
         }
         return "";
-
     }
 };
