39eb95e615ad3b8ce5ed38b1448d042f67c700c0
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-02-17 12:35:13 +0530

    Time: 80 ms (53.43%) | Memory: 57.2 MB (53.87%) - LeetSync

---
+++ 1762-furthest-building-you-can-reach/furthest-building-you-can-reach.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
+        priority_queue<int> bricksUsed;
+        int n = heights.size();
+        
+        for (int i = 0; i < n - 1; i++) {
+            int diff = heights[i + 1] - heights[i];
+            if (diff > 0) {
+                bricksUsed.push(-diff);
+                if (bricksUsed.size() > ladders) {
+                    bricks += bricksUsed.top(); // Add bricks used to bricks available
+                    bricksUsed.pop(); // Use a ladder
+                }
+                if (bricks < 0) {
+                    return i; // Can't go further, return the current index
+                }
+            }
+        }
+        
+        return n - 1; // Reachable till the last building
+    }
+};
