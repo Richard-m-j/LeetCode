cd490f84dd1d9ec0815411a439ac1d77cbdab64b
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-02-20 07:34:27 +0530

    Time: 12 ms (74.59%) | Memory: 20.2 MB (54.51%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,24 +1,34 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
         int n = nums.size();
-        nums.push_back(10001);
-        bool zero = false;
-        for(int i =0;i<n;i++)
-        {
-            int num = abs(nums[i]);
-            if(nums[num])
-                nums[num] *= -1;
-            else
-                zero = true;
-        }
-        for(int i =0;i<n;i++)
-            if(nums[i] == 0 && !zero)
-                return i;
-            else if(nums[i] > 0)
-                return i;
-        return n;
+        int sum = (n * (n + 1)) / 2;
+        for (int i = 0; i < n; i++)
+            sum -= nums[i];
+        return sum;
     }
 };
+// class Solution {
+// public:
+//     int missingNumber(vector<int>& nums) {
+//         ios_base::sync_with_stdio(false);
+//         cin.tie(NULL);
+//         int n = nums.size();
+//         nums.push_back(10001);
+//         bool zero = false;
+//         for(int i =0;i<n;i++)
+//         {
+//             int num = abs(nums[i]);
+//             if(nums[num])
+//                 nums[num] *= -1;
+//             else
+//                 zero = true;
+//         }
+//         for(int i =0;i<n;i++)
+//             if(nums[i] == 0 && !zero)
+//                 return i;
+//             else if(nums[i] > 0)
+//                 return i;
+//         return n;
+//     }
+// };
