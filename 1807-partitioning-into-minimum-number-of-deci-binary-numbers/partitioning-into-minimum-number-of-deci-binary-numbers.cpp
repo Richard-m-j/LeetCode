1727a502db36489280eb4a9faaaf58f1bfda6eb3
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-03-01 06:57:28 +0530

    Time: 353 ms (5.09%) | Memory: 14.9 MB (9.99%) - LeetSync

---
+++ 1807-partitioning-into-minimum-number-of-deci-binary-numbers/partitioning-into-minimum-number-of-deci-binary-numbers.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minPartitions(string n) {
+        sort(n.begin(),n.end());
+        int ans = n[0] - '0';
+        for(int i=1;i<n.length();i++){
+            ans += n[i] - n[i-1];
+            cout<<ans<<" ";
+        }
+        return ans;
+    }
+};
