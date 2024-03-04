9cff0e198ebef8408456a4152b243ba5064a80d1
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-03-04 09:23:32 +0530

    Time: 5 ms (42.42%) | Memory: 13.2 MB (23.53%) - LeetSync

---
+++ 985-bag-of-tokens/bag-of-tokens.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int bagOfTokensScore(vector<int>& tokens, int power) {
+        sort(tokens.begin(),tokens.end());
+
+        int l=0,r=tokens.size()-1;
+        int maxScore = 0,currentScore =0;
+        while(l<=r){
+            if(tokens[l]<=power){
+                //face-up
+                currentScore++;
+                power -= tokens[l];
+                l++;
+            }
+            else if(currentScore >= 1){
+                //face-down
+                currentScore--;
+                power += tokens[r];
+                r--;
+            }
+            else
+                break;
+            maxScore = max(maxScore,currentScore);
+        }
+        return maxScore;
+    }
+};
