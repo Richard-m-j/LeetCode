52059f7faff790441a47a882453a6edd3ab26b30
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-03-05 12:00:54 +0530

    Time: 11 ms (99.66%) | Memory: 14 MB (13.45%) - LeetSync

--- 1850-minimum-length-of-string-after-deleting-similar-ends/minimum-length-of-string-after-deleting-similar-ends.cpp
+++ 1850-minimum-length-of-string-after-deleting-similar-ends/minimum-length-of-string-after-deleting-similar-ends.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int minimumLength(string s) {
         ios_base::sync_with_stdio(0);
+        cin.tie(0);
         int left = 0, right = s.length() - 1;
 
         while(left < right) {
