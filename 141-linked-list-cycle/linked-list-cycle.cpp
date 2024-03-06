49097ab346ee5ee5be9e6a862226fd9a556567da
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-03-06 06:26:19 +0530

    Time: 5 ms (90.85%) | Memory: 10.5 MB (53.92%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -9,6 +9,8 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
+        ios::sync_with_stdio(0);
+        cin.tie(0);
         ListNode *ptr = head;
         while(ptr){
             if(ptr->val == 100001)
