b2b281a8f57464b75612f4fb1e970f0537e6a5a7
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-03-09 07:06:36 +0530

    Time: 39 ms (98.86%) | Memory: 52.6 MB (85.74%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.cpp
+++ 2634-minimum-common-value/minimum-common-value.cpp
@@ -4,8 +4,8 @@ public:
         ios::sync_with_stdio(0);
         cin.tie(0);
         cout.tie(0);
-        if(nums1[nums1.size() -1] < nums2[0] || nums2[nums2.size() -1] < nums1[0])
-            return -1;
+        // if(nums1[nums1.size() -1] < nums2[0] || nums2[nums2.size() -1] < nums1[0])
+        //     return -1;
         int i=0,j=0;
         while(i<nums1.size() && j<nums2.size()){
             if(nums1[i] == nums2[j])
