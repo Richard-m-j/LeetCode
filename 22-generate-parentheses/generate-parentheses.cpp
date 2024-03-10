bbd44f33d25578bd7b940e80d8d3047223cbf51b
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-03-10 07:32:14 +0530

    Time: 4 ms (46.27%) | Memory: 15.1 MB (62.05%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> res;
+        helper(res, "(", 1, 0, n);
+        return res;
+    }
+    void helper(vector<string>& res, string s, int l, int r, int n) {
+        if (l == r && r == n) {
+            res.push_back(s);
+            return;
+        }
+        if (l < n)
+            helper(res, s + '(', l + 1, r, n);
+        if (r < l)
+            helper(res, s + ')', l, r + 1, n);
+    }
+};
