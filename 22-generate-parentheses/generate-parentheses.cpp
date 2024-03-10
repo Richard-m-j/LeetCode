e0ad8a68df865a04a4ce033ca4d8e59f4529ca8f
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-03-10 07:38:05 +0530

    Time: 7 ms (19.54%) | Memory: 15.1 MB (53.76%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -2,24 +2,17 @@ class Solution {
 public:
     vector<string> generateParenthesis(int n) {
         vector<string> res;
-        stack<pair<string, pair<int, int>>> stk;
-        stk.push({"(", {1, 0}});
-
-        while (!stk.empty()) {
-            auto [s, counts] = stk.top();
-            stk.pop();
-            int l = counts.first, r = counts.second;
-
-            if (l == n && r == n) {
-                res.push_back(s);
-            } else {
-                if (l < n)
-                    stk.push({s + '(', {l + 1, r}});
-                if (r < l)
-                    stk.push({s + ')', {l, r + 1}});
-            }
-        }
-
+        helper(res, "(", 1, 0, n);
         return res;
+    }
+    void helper(vector<string>& res, string s, int l, int r, int n) {
+        if (l == r && r == n) {
+            res.push_back(s);
+            return;
+        }
+        if (l < n)
+            helper(res, s + '(', l + 1, r, n);
+        if (r < l)
+            helper(res, s + ')', l, r + 1, n);
     }
 };
