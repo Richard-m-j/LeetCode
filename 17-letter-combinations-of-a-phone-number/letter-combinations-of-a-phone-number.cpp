1fc1156741d08efec043d52e1bf91605e3dc6e5f
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-03-11 16:59:18 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (18.00%) - LeetSync

---
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,27 @@
+class Solution {
+private:
+    vector<string> res;
+    string digits;
+    vector<string> keyboard = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+public:
+    vector<string> letterCombinations(string digits) {
+        Solution::digits = digits;
+        if(digits.size() == 0)
+            return {};
+        helper("");
+        return res;
+    }
+    void helper(string s){
+        if(s.size() == digits.size()){
+            res.push_back(s);
+            return;
+        }
+        string sequence = keyboard[digits[s.size()] - '0'];
+        // string sequence = "abc";
+        // int digit = digits[s.size()];
+        // cout<<digit<<keyboard[3]<<" ";
+        for(int i=0;i<sequence.size();i++){
+            helper(s+sequence[i]);
+        }
+    }
+};
