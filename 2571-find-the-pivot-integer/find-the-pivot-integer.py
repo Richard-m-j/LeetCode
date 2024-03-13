aa81808f1c38354203929718a2831c4c46f5bf62
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-03-13 06:55:16 +0530

    Time: 38 ms (85.89%) | Memory: 16.5 MB (59.17%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.py
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.py
@@ -1,18 +1,8 @@
 class Solution:
     def pivotInteger(self, n: int) -> int:
-        c = (n*(n+1))//2
+        c = pow((n*(n+1))//2,0.5)
+        convert = int(c)
+        return convert if convert == c else -1
 
-        l=1
-        r=n
-        while l<=r:
-            m = (l+r)//2
-            leftSum = (m*(m+1))//2
-            rightSum = c - leftSum + m
-            if leftSum == rightSum:
-                return m
-            elif leftSum < rightSum:
-                l = m+1
-            else:
-                r=m-1
-        return -1
+        
 
