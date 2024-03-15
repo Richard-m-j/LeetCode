0c8a0ffe36463fa0fdd88b399d8513ad852e72db
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-03-15 06:56:55 +0530

    Time: 174 ms (29.38%) | Memory: 23.8 MB (77.62%) - LeetSync

---
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -0,0 +1,15 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        prefix = [1] * len(nums)
+
+        for i in range(0, len(nums) - 1):
+            prefix[i + 1] = prefix[i] * nums[i]
+        suffix = [1] * len(nums)
+
+        for i in range(len(nums) - 2, -1, -1):
+            suffix[i] = suffix[i+1] * nums[i+1]
+        
+        for i in range(len(nums)):
+            nums[i] = prefix[i] * suffix[i]
+            
+        return nums
