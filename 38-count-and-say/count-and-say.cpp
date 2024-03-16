8ebd14a1e1d9119011334ff4427083e7d4c7eb23
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-03-16 18:07:26 +0530

    Time: 3 ms (71.02%) | Memory: 8.6 MB (83.20%) - LeetSync

---
+++ 38-count-and-say/count-and-say.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string countAndSay(int n) {
+        vector<string> dp(n);
+        dp[0] = "1";
+        for (int i = 1; i < n; i++) {
+            // dp[i]
+            string s = dp[i - 1];
+            s += '/';
+            int count = 1;
+            string ans = "";
+            cout<<s<<" ";
+            for (int j = 0; j < s.length() - 1; j++) {
+                if (s[j] == s[j + 1])
+                    count++;
+                else {
+                    ans += count + '0';
+                    ans += s[j];
+                    count = 1;
+                }
+            }
+            dp[i] = ans;
+        }
+        return dp[n - 1];
+    }
+};
