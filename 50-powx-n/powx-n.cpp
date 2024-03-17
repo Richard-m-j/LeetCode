bd4b1a07e08b937d121ac775303cc6f5f440dbb4
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-03-17 21:16:59 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (11.88%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -2,28 +2,18 @@ class Solution {
 public:
     double myPow(double x, int n) {
         double res = 1;
-           
-        if(n==0.0)
-            return 1;
-        if(x == 1.0)
-            return 1;
-        if(x == -1.0)
-            if(n%2)
-                return -1;
-            else
-                return 1;
-        if(n==INT_MIN)
-            if(x!=1.0000000000001)
-                return 0;
-            else
-                return 0.99979;
 
-        if(n>0)
-            for(int i=0;i<n;i++)
-                res *= x;
-        else{
-            for(int i=n;i<0;i++)
-                res /= x;
+        if(n < 0) {
+            n = abs(n);
+            x = 1/x;
+        }
+
+        while(n > 0) {
+            if(n%2 != 0) {
+                res = res * x;
+            }
+            x = x*x;
+            n = n/2;
         }
         return res;
     }
