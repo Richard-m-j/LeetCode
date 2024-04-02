7a475a2a63a37b8002cab671903f5a28c454f372
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-04-02 06:43:24 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (94.60%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        vector<char> mp(128,'\0');
+        vector<char> rev(128,'\0');
+
+        if(s.length()!=t.length())
+            return false;
+        for(int i=0;i<s.length();i++){
+            if(mp[s[i]] == '\0' && rev[t[i]] == '\0'){
+                mp[s[i]] = t[i];
+                rev[t[i]] = s[i];
+            }
+            else if(mp[s[i]] != t[i])
+                return false;
+        }
+        return true;
+    }
+};
