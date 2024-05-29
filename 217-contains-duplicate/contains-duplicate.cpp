f0d0c591609e17c56796f8d91dc44ca48627e927
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-05-29 17:58:47 +0530

    Time: 63 ms (99.06%) | Memory: 73.2 MB (44.25%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         unordered_map<int,int> mp;
         for(auto num: nums)
             if(!mp[num])
