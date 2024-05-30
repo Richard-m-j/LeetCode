598ad785500690262d1812968ced5879426c32b6
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-05-30 19:20:28 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (23.82%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     bool isValid(string s) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         stack<char> stack;
         for(auto &c: s){
             if(c== '(' || c=='{' || c=='[')
