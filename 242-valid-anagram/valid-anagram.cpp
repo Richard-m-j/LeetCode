0273b736dd012b081108e8405726ff7cc7442809
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-05-31 07:24:28 +0530

    Time: 11 ms (37.71%) | Memory: 9.7 MB (19.47%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length() != t.length())
+            return false;
+        unordered_map<char,int> m1,m2;
+        for(auto &c: s)
+            m1[c]++;
+        for(auto &c: t)
+            m2[c]++;
+        for(auto &it: m1)
+            if(it.second != m2[it.first])
+                return false;
+        return true;
+    }
+};
