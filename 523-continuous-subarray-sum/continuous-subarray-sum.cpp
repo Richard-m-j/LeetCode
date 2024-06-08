15ba65ce31f8249bd4bc967a0ba2b30411c931c6
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-06-09 00:13:12 +0530

    Time: 234 ms (17.70%) | Memory: 134.7 MB (59.36%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.cpp
+++ 523-continuous-subarray-sum/continuous-subarray-sum.cpp
@@ -3,13 +3,12 @@ public:
     bool checkSubarraySum(std::vector<int>& nums, int k) {
         if (nums.size() < 2)
             return false;
-        std::unordered_map<int,int> mp;
+        unordered_map<int,int> mp;
         mp[0] = -1;
         int accumulated = 0;
         for (int i = 0; i < nums.size(); i++){
             accumulated += nums[i];
             int mod = accumulated % k;
-            if(mod < 0) mod += k; 
             if(mp.find(mod) != mp.end()){
                 if(i - mp[mod] > 1)
                     return true;
