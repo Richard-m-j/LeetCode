321529c5183ae52ec46710a3970f736c423c7d5c
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-06-27 20:12:06 +0530

    Time: 14 ms (32.43%) | Memory: 13.3 MB (97.38%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.cpp
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -1,44 +1,39 @@
 class Solution {
 private:
-    vector<int> dir = {-1, 0, 1, 0, -1};
     int m, n;
 
 public:
     void solve(vector<vector<char>>& board) {
-        if (board.empty() || board[0].empty()) return;
+        if (board.empty() || board[0].empty())
+            return;
         m = board.size();
         n = board[0].size();
-        vector<vector<bool>> visited(m, vector<bool>(n, false));
-        
-        // Run DFS for 'O's on the border and mark them as visited
+
         for (int i = 0; i < m; i++) {
-            if (board[i][0] == 'O') dfs(board, visited, i, 0);
-            if (board[i][n - 1] == 'O') dfs(board, visited, i, n - 1);
+            dfs(board, i, 0);
+            dfs(board, i, n - 1);
         }
         for (int j = 0; j < n; j++) {
-            if (board[0][j] == 'O') dfs(board, visited, 0, j);
-            if (board[m - 1][j] == 'O') dfs(board, visited, m - 1, j);
+            dfs(board, 0, j);
+            dfs(board, m - 1, j);
         }
-
-        // Flip unvisited 'O's to 'X's and visited 'O's back to 'O'
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (!visited[i][j] && board[i][j] == 'O') {
+                if (board[i][j] == '*')
+                    board[i][j] = 'O';
+                else
                     board[i][j] = 'X';
-                }
             }
         }
     }
 
-    void dfs(vector<vector<char>>& board, vector<vector<bool>>& visited, int row, int col) {
-        if (visited[row][col]) return;
-        visited[row][col] = true;
-        for (int d = 0; d < 4; d++) {
-            int newR = row + dir[d];
-            int newC = col + dir[d + 1];
-            if (newR >= 0 && newR < m && newC >= 0 && newC < n && board[newR][newC] == 'O') {
-                dfs(board, visited, newR, newC);
-            }
-        }
+    void dfs(vector<vector<char>>& board, int row, int col) {
+        if (row<0 || row>=board.size() || col<0 || col>=board[0].size() || board[row][col]!='O')
+            return;
+        board[row][col] = '*';
+        dfs(board, row - 1, col);
+        dfs(board, row + 1, col);
+        dfs(board, row, col - 1);
+        dfs(board, row, col + 1);
     }
 };
