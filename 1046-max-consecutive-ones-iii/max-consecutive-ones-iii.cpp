c41ac6001840d2cd05d744817a7c02c50cd4994f
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-07-28 11:33:08 +0530

    Time: 31 ms (93.35%) | Memory: 58.2 MB (19.48%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
@@ -3,16 +3,16 @@ public:
     int longestOnes(vector<int>& nums, int k) {
         ios::sync_with_stdio(0);
         cin.tie(0);
-        int flip=k,l=0,r=0,res=0;
-        while(r<nums.size()){
-            if(!nums[r]){
-                if(flip)
+        int flip = k, l = 0, r = 0, res = 0;
+        while (r < nums.size()) {
+            if (!nums[r])
+                if (flip)
                     flip--;
-                else{
-                    while(nums[l++]);
-                }
-            }
-            res = max(res,r-l+1);
+                else
+                    while (nums[l++])
+                        ;
+
+            res = max(res, r - l + 1);
             r++;
         }
         return res;
