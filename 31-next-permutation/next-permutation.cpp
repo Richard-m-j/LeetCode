c967f229307bcac2416dcaad4fd30522c46ffa33
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-08-14 21:03:51 +0530

    Time: 10 ms (14.86%) | Memory: 14.9 MB (31.06%) - LeetSync

---
+++ 31-next-permutation/next-permutation.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        int l = 0,r=n-1;
+        for(int i=n-2; i>-1;i--){
+            if(nums[i]<nums[i+1]){
+                l = i+1;
+                for(int j=r;j>i;j--){
+                    if(nums[j]>nums[i]){
+                        swap(nums[i],nums[j]);
+                        break;
+                    }
+                }   
+                break;
+            }
+        }
+        cout<<l<<" "<<r<<endl;
+        while(l<r){
+            swap(nums[l],nums[r]);
+            l++;
+            r--;
+        }      
+    }
+};
