66e6277862f5b7cd863e7bdb36875a36aa4e8581
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-08-30 07:58:42 +0530

    Time: 12 ms (85.89%) | Memory: 18.3 MB (65.42%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -10,26 +10,28 @@
  */
 class Solution {
 public:
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
-        ios::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
-        vector<int> temp;
-        for(auto& list: lists){
-            ListNode* ptr = list;
-            while(ptr){
-                temp.push_back(ptr->val);
-                ptr = ptr->next;
+    static bool heapComp(ListNode* a, ListNode* b) { return a->val > b->val; }
+    ListNode* mergeKLists(vector<ListNode*>& lists) { // make_heap
+        ListNode head(0);
+        ListNode* curNode = &head;
+        vector<ListNode*> v;
+        for (int i = 0; i < lists.size(); i++) {
+            if (lists[i])
+                v.push_back(lists[i]);
+        }
+        make_heap(v.begin(), v.end(), heapComp); // vector -> heap data strcture
+
+        while (v.size() > 0) {
+            curNode->next = v.front();
+            pop_heap(v.begin(), v.end(), heapComp);
+            v.pop_back();
+            curNode = curNode->next;
+            if (curNode->next) {
+                v.push_back(curNode->next);
+                push_heap(v.begin(), v.end(), heapComp);
             }
         }
-        sort(temp.begin(),temp.end());
-        ListNode *head = new ListNode();
-
-        ListNode* ptr = head;
-        for(int& num: temp){
-            ptr->next = new ListNode(num);
-            ptr = ptr->next;
-        }
-        return head->next;
+        return head.next;
     }
-};
+}
+;
