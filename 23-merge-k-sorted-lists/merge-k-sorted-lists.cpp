844a30ba2e9fbe658bb86837e952655dd50759f6
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-08-30 07:49:51 +0530

    Time: 17 ms (56.56%) | Memory: 19.1 MB (11.31%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -11,40 +11,22 @@
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        ios::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
-        ListNode *head = NULL;
-
-        auto merge2 = [](ListNode *list1, ListNode *list2){
-            if(!list1)
-                return list2;
-            if(!list2)
-                return list1;
-            ListNode *head = new ListNode();
-            ListNode *ptr = head;
-            while(list1 && list2){
-                if(list1->val <= list2->val){
-                    ptr->next = list1;
-                    list1 = list1->next;
-                }
-                else{
-                    ptr->next = list2;
-                    list2 = list2->next;
-                }
+        vector<int> temp;
+        for(auto& list: lists){
+            ListNode* ptr = list;
+            while(ptr){
+                temp.push_back(ptr->val);
                 ptr = ptr->next;
             }
-            if(list1)
-                ptr->next = list1;
-            else
-                ptr->next = list2;
-
-            return head->next;
-        };
-        for(auto& list: lists){
-            head = merge2(head,list);
         }
-        
-        return head;
+        sort(temp.begin(),temp.end());
+        ListNode *head = new ListNode();
+
+        ListNode* ptr = head;
+        for(int& num: temp){
+            ptr->next = new ListNode(num);
+            ptr = ptr->next;
+        }
+        return head->next;
     }
 };
