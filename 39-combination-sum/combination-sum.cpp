0c0de404ff3b55fcd92e5d08eefff611026377f6
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-08-31 11:41:48 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (50.06%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,22 +1,29 @@
+static const auto speedUp = []() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    return 0;
+}();
+
 class Solution {
 private:
     vector<vector<int>> res;
     vector<int> current;
 
-    void helper(vector<int>& candidates, int target, int n){
-        if(n<0 || target <0)
+    void helper(vector<int>& candidates, int target, int n) {
+        if (n < 0 || target < 0)
             return;
-        if(target == 0)
+        if (target == 0)
             res.push_back(current);
-        for(int i=n;i>=0;i--){
+        for (int i = n; i >= 0; i--) {
             current.push_back(candidates[i]);
-            helper(candidates,target - candidates[i],i);
+            helper(candidates, target - candidates[i], i);
             current.pop_back();
         }
     }
+
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        helper(candidates,target,candidates.size()-1);
+        helper(candidates, target, candidates.size() - 1);
         return res;
     }
 };
