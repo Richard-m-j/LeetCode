2b4b9ff5f8c909da6e1dd440dd83fb23763d6d4f
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-08-31 09:47:58 +0530

    Time: 246 ms (47.81%) | Memory: 94.9 MB (29.98%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.cpp
+++ 435-non-overlapping-intervals/non-overlapping-intervals.cpp
@@ -1,32 +1,24 @@
 class Solution {
 public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-        //sort the intervals
-        sort(intervals.begin(),intervals.end(),[](const auto &a, const auto &b){
-        if(a[0] != b[0])
-            return a[0] < b[0];
-        return a[1] < b[1];
-    });
-        for(int i=0;i<intervals.size();i++){
-            cout<<intervals[i][0]<<","<<intervals[i][1]<<" ";
-        }
-        cout<<endl;
-        vector<int> dp(intervals.size(),0);
+        // sort the intervals
+        sort(intervals.begin(), intervals.end(),
+             [](const auto& a, const auto& b) {
+                 if (a[0] != b[0])
+                     return a[0] < b[0];
+                 return a[1] < b[1];
+             });
+        vector<int> dp(intervals.size(), 0);
         int res = 0;
         int l = intervals[0][0];
         int r = intervals[0][1];
-    
-        for(int i=1;i<intervals.size();i++){
-            if(intervals[i][0]==l){
+
+        for (int i = 1; i < intervals.size(); i++) {
+            if (intervals[i][0] == l || intervals[i][0] < r) {
+                r = min(r, intervals[i][1]);
                 res++;
-                cout<<intervals[i][0]<<" "<<intervals[i][1]<<endl;
-            }
-            else if(intervals[i][0]<r){
-                r = min(r,intervals[i][1]);
-                res++;
-            }
-            else{
-                l  = intervals[i][0];
+            } else {
+                l = intervals[i][0];
                 r = intervals[i][1];
             }
         }
