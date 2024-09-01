42d4806258e4adab77e347d804cbb9dc95da9475
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-09-01 09:41:06 +0530

    Time: 3 ms (75.72%) | Memory: 13 MB (41.73%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,31 +1,25 @@
-static const auto speedUp = [](){
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    return 0;
-}();
 class Solution {
 private:
-    void helper(vector<int>& candidates, int target,vector<vector<int>>& combinations,vector<int>& current, int n){
-        if(target == 0){
-            combinations.push_back(current);
+    void backtracking(vector<int>& candidates, vector<vector<int>>& result, vector<int>& path, int idx, int target) {
+        if (target == 0) {
+            result.push_back(path);
             return;
         }
-        for(int i=n;i<candidates.size();i++){
-            if(i>n && candidates[i] == candidates[i-1])
-                continue;
-            if(candidates[i] > target)
-                break;
-            current.push_back(candidates[i]);
-            helper(candidates,target - candidates[i],combinations,current,i+1);
-            current.pop_back();
+
+        for (int i = idx; i < candidates.size(); i++) {
+            if (i > idx && candidates[i] == candidates[i - 1]) continue;
+            if (candidates[i] > target) break;
+            path.push_back(candidates[i]);
+            backtracking(candidates, result, path, i + 1, target - candidates[i]);
+            path.pop_back();
         }
     }
 public:
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        sort(candidates.begin(),candidates.end());
-        vector<vector<int>> res;
-        vector<int> current;
-        helper(candidates,target,res,current,0);
-        return res;
+        vector<vector<int>> result;
+        vector<int> path;
+        sort(candidates.begin(), candidates.end());
+        backtracking(candidates, result, path, 0, target);
+        return result;
     }
 };
