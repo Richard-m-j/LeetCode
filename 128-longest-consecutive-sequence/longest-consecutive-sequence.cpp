5550e30a70c89875736f3ddbd482c8b78109c6d0
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-09-02 07:42:22 +0530

    Time: 304 ms (27.50%) | Memory: 99.6 MB (5.03%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -20,38 +20,24 @@ private:
             }
         }
     }
-    // void connect(int a, int b){
-    // int bParent = find(b);
-    // int aParent = find(a);
-    // if(aParent != bParent){
-    //     if(size[aParent] < size[bParent]){
-    //         parent[aParent] = bParent; // Make bParent the parent of aParent
-    //         size[bParent] += size[aParent]; // CORRECTION: Add the size of
-    //         aParent's set to bParent's set
-    //     } else {
-    //         parent[bParent] = aParent; // Make aParent the parent of bParent
-    //         size[aParent] += size[bParent]; // CORRECTION: Add the size of
-    //         bParent's set to aParent's set
-    //     }
-    // }
 
-
-public : int
-         longestConsecutive(vector<int>& nums) {
-    for (int& num : nums) {
-        if (parent.find(num) == parent.end()) {
-            parent[num] = num;
-            size[num] = 1;
+public:
+    int longestConsecutive(vector<int>& nums) {
+        for (int& num : nums) {
+            if (parent.find(num) == parent.end()) {
+                parent[num] = num;
+                size[num] = 1;
+            }
+            if (parent.find(num + 1) != parent.end() &&
+                find(num) != find(num + 1))
+                connect(num + 1, num);
+            if (parent.find(num - 1) != parent.end() &&
+                find(num) != find(num - 1))
+                connect(num, num - 1);
         }
-        if (parent.find(num + 1) != parent.end() && find(num) != find(num + 1))
-            connect(num + 1, num);
-        if (parent.find(num - 1) != parent.end() && find(num) != find(num - 1))
-            connect(num, num - 1);
+        int res = 0;
+        for (auto& it : size)
+            res = max(res, it.second);
+        return res;
     }
-    int res = 0;
-    for (auto& it : size)
-        res = max(res, it.second);
-    return res;
-}
-}
-;
+};
