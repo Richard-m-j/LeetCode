e9185074f4d74bc6353241db5eb15ab9ffa6a0ab
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-09-05 08:18:18 +0530

    Time: 16 ms (36.70%) | Memory: 20.7 MB (5.65%) - LeetSync

---
+++ 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.cpp
@@ -0,0 +1,46 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        //perform bfs
+        if(!root)
+            return root;
+        queue<Node*> q;
+        q.push(root);
+        while(!q.empty()){
+            queue<Node*> q1;
+            Node *prev = new Node();
+            while(!q.empty()){
+                Node *ptr = q.front();
+                q.pop();
+                prev->next = ptr;
+                // cout<<ptr->val<<" ";
+                prev = ptr;
+                if(ptr->left){
+                    q1.push(ptr->left);
+                    q1.push(ptr->right);
+                }
+            }
+            prev->next = nullptr;
+            q = q1;
+        }
+        return root;
+    }
+};
