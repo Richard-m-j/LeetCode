b25620ce9f74e440318a9e8b11573307486eee8b
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-09-05 19:51:03 +0530

    Time: 124 ms (11.91%) | Memory: 18.7 MB (43.01%) - LeetSync

---
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n=matrix.size();
+        int m=matrix[0].size();
+
+        for(int i=0;i<n;i++){
+            if(matrix[i][0]>target)
+                return false;
+            int l = lower_bound(matrix[i].begin(),matrix[i].end(),target) - matrix[i].begin();
+            if(l<m && matrix[i][l] == target)
+                return true;
+        }
+        return false;
+    }
+};
