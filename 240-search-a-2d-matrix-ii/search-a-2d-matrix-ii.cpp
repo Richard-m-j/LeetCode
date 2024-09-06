e9e97e8f438d2520ead3fc377de571a46dc462fb
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-09-06 06:11:53 +0530

    Time: 101 ms (16.41%) | Memory: 17.9 MB (43.64%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
@@ -3,14 +3,25 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         ios::sync_with_stdio(0);
         cin.tie(0);
-        int n=matrix.size();
-        int m=matrix[0].size();
-
-        for(int i=0;i<n;i++){
-            if(matrix[i][0]>target)
-                return false;
-            int l = lower_bound(matrix[i].begin(),matrix[i].end(),target) - matrix[i].begin();
-            if(l<m && matrix[i][l] == target)
+        int n = matrix.size();
+        int m = matrix[0].size();
+        // find upper bound row
+        int l = 0, r = n - 1;
+        while (l < r) {
+            int m = (l+r)/ 2;
+            if (matrix[m][0] == target)
+                return true;
+            if (matrix[m][0] > target)
+                r = m - 1;
+            else
+                l = m + 1;
+        }
+        // cout<<l<<endl;
+        for (int i = l; i >= 0; i--) {
+            int mid = lower_bound(matrix[i].begin(), matrix[i].end(), target) - matrix[i].begin();
+            // if(mid<m)
+            //     cout<<i<<" "<<mid<<" "<<matrix[i][mid]<<endl;
+            if (mid < m && matrix[i][mid] == target)
                 return true;
         }
         return false;
