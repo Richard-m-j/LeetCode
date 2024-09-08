daa029252ac81518cb5034ad2aaf6b7cde35b9ef
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-09-08 09:51:46 +0530

    Time: 3 ms (95.24%) | Memory: 13.6 MB (66.24%) - LeetSync

--- 674-longest-continuous-increasing-subsequence/longest-continuous-increasing-subsequence.cpp
+++ 674-longest-continuous-increasing-subsequence/longest-continuous-increasing-subsequence.cpp
@@ -1,8 +1,10 @@
 class Solution {
 public:
     int findLengthOfLCIS(vector<int>& nums) {
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+
         int res = 1, curr = 1;
-        ;
         for (int i = 1; i < nums.size(); i++)
             if (nums[i] > nums[i - 1]) {
                 curr++;
