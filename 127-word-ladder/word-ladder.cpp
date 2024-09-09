e7562f7ec2b8bd11df566a96ba46f48e77d8dd74
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-09-09 08:32:09 +0530

    Time: 320 ms (31.78%) | Memory: 56.8 MB (5.80%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -4,31 +4,48 @@ public:
         ios::sync_with_stdio(0);
         cin.tie(0);
         unordered_set<string> visited;
-        unordered_set<string> words;
+        unordered_map<string,vector<string>> wordMap;
+        unordered_set<string> wordSet;
         queue<string> q;
-        for(string& s: wordList)
-            words.insert(s);
         int n = beginWord.length();
+        wordList.push_back(beginWord);
+        for(string& s: wordList)
+            wordSet.insert(s);
+        for(auto& s: wordList){
+            vector<string> branches;
+            for(int i=0;i<n;i++){
+                string nextWord = s;
+                for(int j=0;j<26;j++){
+                    nextWord[i] = j + 'a';
+                    if(nextWord == s)
+                        continue;
+                    if(wordSet.find(nextWord)!=wordSet.end()){
+                        branches.push_back(nextWord);
+                        cout<<s<<"-"<<nextWord<<" ";
+                    }
+                }
+            }
+            wordMap[s] = branches;
+            cout<<s<<endl;
+        }
         q.push(beginWord);
         int level=1;
         while(!q.empty()){
             queue<string> q1;
             while(!q.empty()){
                 string curr = q.front();
-                // cout<<curr<<" ";
+                cout<<curr<<" ";
                 q.pop();
-                for(int i=0;i<n;i++){
-                    string next = curr;
-                    for(int j=0;j<26;j++){
-                        next[i] = j+'a';
-                        if(words.find(next)!=words.end() && visited.find(next)==visited.end())
-                            if(next == endWord)
-                                return level+1;
-                            else{
-                                q1.push(next);
-                                visited.insert(next);
-                            }
-                    }
+                vector<string> branches = wordMap[curr];
+                cout<<branches.size()<<endl;
+                for(auto& next: branches){
+                    cout<<next<<" ";
+                    if(visited.find(next) != visited.end())
+                        continue;
+                    if(next == endWord)
+                        return level+1;
+                    q1.push(next);
+                    visited.insert(next);
                 }
             }
             q=q1;
