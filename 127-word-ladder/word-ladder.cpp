aa9a15036ccb253880fcf67086ca58b8bff910eb
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-09-09 08:47:33 +0530

    Time: 62 ms (80.48%) | Memory: 21.7 MB (25.19%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -3,7 +3,6 @@ public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
         ios::sync_with_stdio(0);
         cin.tie(0);
-        unordered_set<string> visited;
         unordered_set<string> words(wordList.begin(),wordList.end());
         queue<string> q;
         // for(string& s: wordList)
