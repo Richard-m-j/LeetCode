10bba62ab567cf9875aede392f81c4cb0806b30c
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-09-12 08:08:33 +0530

    Time: 7 ms (51.97%) | Memory: 11 MB (71.61%) - LeetSync

---
+++ 47-permutations-ii/permutations-ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+private:
+    int n;
+    void helper(unordered_map<int,int>& mp,vector<vector<int>>& res,vector<int>& current){
+        if(current.size() == n)
+            res.push_back(current);
+        for(auto& it:mp){
+            if(it.second){
+                current.push_back(it.first);
+                cout<<it.first<<" ";
+                mp[it.first]--;
+                helper(mp,res,current);
+                mp[it.first]++;
+                current.pop_back();
+            }
+        }
+    }
+public:
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        n = nums.size();
+        unordered_map<int,int> mp;
+        for(int& num: nums)
+            mp[num]++;
+        vector<vector<int>> res;
+        vector<int> current;
+        helper(mp,res,current);
+        return res;
+        
+    }
+};
