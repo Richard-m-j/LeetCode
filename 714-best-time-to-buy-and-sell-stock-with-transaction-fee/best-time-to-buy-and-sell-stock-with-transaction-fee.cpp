98097808c622e9cf43082cfedba926297d3308c4
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-09-16 12:52:18 +0530

    Time: 32 ms (99.81%) | Memory: 57.2 MB (100.00%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices, int fee) {
+        ios::sync_with_stdio(0);
+        cin.tie(0);
         int buy = INT_MIN;
         int sell = 0;
         for(int& price: prices){
