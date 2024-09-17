e7d89c3320693346aaebb7ed2f6c1ca3284e0693
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-09-17 09:02:11 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (89.02%) - LeetSync

---
+++ 920-uncommon-words-from-two-sentences/uncommon-words-from-two-sentences.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<string> uncommonFromSentences(string s1, string s2) {
+        unordered_map<string,int> mp;
+        string word = "";
+        for(int i=0;i<s1.length();i++){
+            if(s1[i] == ' '){
+                mp[word]++;
+                word = "";
+            }
+            else
+                word += s1[i];
+        }
+        mp[word]++;
+        word = "";
+        for(int i=0;i<s2.length();i++){
+            if(s2[i] == ' '){
+                mp[word]++;
+                word = "";
+            }
+            else
+                word += s2[i];
+        }
+        mp[word]++;
+        vector<string> res;
+        for(auto& it: mp)
+            if(it.second == 1)
+                res.push_back(it.first);
+        return res;
+    }
+};
