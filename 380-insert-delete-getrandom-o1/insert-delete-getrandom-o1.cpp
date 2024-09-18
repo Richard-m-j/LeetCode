236003f6a0d7c7e94dcedb81c600866b3b5561b7
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-09-18 17:48:31 +0530

    Time: 144 ms (95.28%) | Memory: 103.7 MB (6.60%) - LeetSync

---
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.cpp
@@ -0,0 +1,39 @@
+int speedUp = []{ios::sync_with_stdio(0); cin.tie(0); return 0;}();
+class RandomizedSet {
+private:
+    unordered_set <int> randomSet;
+public:
+    RandomizedSet() {
+        
+    }
+    
+    bool insert(int val) {
+        if(randomSet.find(val) != randomSet.end())
+            return false;
+        randomSet.insert(val);
+        return true;
+    }
+    
+    bool remove(int val) {
+        if(randomSet.find(val) == randomSet.end())
+            return false;
+        randomSet.erase(val);
+        return true;
+    }
+    
+    int getRandom() {
+        int n = rand() % randomSet.size();
+
+        auto it = begin(randomSet);
+        advance(it, n);
+        return *it;
+    }
+};
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet* obj = new RandomizedSet();
+ * bool param_1 = obj->insert(val);
+ * bool param_2 = obj->remove(val);
+ * int param_3 = obj->getRandom();
+ */
