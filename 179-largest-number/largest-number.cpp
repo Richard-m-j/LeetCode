843b263408108e4ae5ef0a43233249b557f3342b
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-09-18 10:10:02 +0530

    Time: 7 ms (61.31%) | Memory: 17.4 MB (36.02%) - LeetSync

--- 179-largest-number/largest-number.cpp
+++ 179-largest-number/largest-number.cpp
@@ -5,16 +5,8 @@ public:
         bool foundZero = false;
         for(auto&num:nums)
             numStrings.push_back(to_string(num));
-        sort(numStrings.rbegin(),numStrings.rend(),[](auto& a,auto& b){
-            int k=0;
-            for(int i=0,j=0;i<a.length()&&j<b.length();i=(i+1)%a.length(),j=(j+1)%b.length()){
-                if(k>2*a.length()||k>2*b.length())
-                    return a.length()>b.length();
-                if(a[i]!=b[j])
-                    return a[i]<b[j];
-                k++;
-            }
-            return false;
+        sort(numStrings.begin(),numStrings.end(),[](auto& a, auto& b){
+            return a+b > b+a;
         });
 
         string res = "";
