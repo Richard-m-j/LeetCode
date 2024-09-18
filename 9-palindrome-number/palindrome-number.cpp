4a3328745a95b1d73f587a7ca1a2ab4e4525c8b5
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-09-18 18:21:47 +0530

    Time: 4 ms (84.87%) | Memory: 8.7 MB (26.06%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+        if(x<0)
+            return false;
+        long n = x, r = 0;
+        while(n){
+            r = r*10 + n%10;
+            n/=10;
+        }
+        return r == x;
+    }
+};
