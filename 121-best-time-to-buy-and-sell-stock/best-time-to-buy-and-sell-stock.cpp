6f74072f76b4ee2e5bbca0b9902986ee1035c012
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-09-18 11:06:04 +0530

    Time: 54 ms (98.96%) | Memory: 96.4 MB (6.16%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -4,17 +4,13 @@ public:
         ios_base::sync_with_stdio(0);
         cin.tie(0);
         cout.tie(0);
-        int buy = 0;
+        int buy = prices[0];
         int sell = 0;
         int profit = 0;
         for (int i = 1; i < prices.size(); i++) {
-            if (prices[i] < prices[buy])
-                buy = i;
-            if (sell < buy || prices[sell] < prices[i])
-                sell = i;
-            profit =
-                sell > buy ? max(profit, prices[sell] - prices[buy]) : profit;
+            sell = max(sell,prices[i] - buy);
+            buy = min(buy,prices[i]);
         }
-        return profit;
+        return sell;
     }
 };
