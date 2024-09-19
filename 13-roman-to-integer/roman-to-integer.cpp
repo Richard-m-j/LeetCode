2c6de1142cdbed55d207c64bbcf7ecfe6c5d63eb
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-09-19 19:48:43 +0530

    Time: 14 ms (35.73%) | Memory: 13 MB (66.38%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map <char,int> chart = {{'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}};
+        int ans = 0;
+        int n = s.length();
+        for(int i=0; i<n;i++)
+        {
+            if(i<n-1 && chart[s[i]] < chart[s[i+1]])
+                ans -= chart[s[i]];
+            else
+                ans += chart[s[i]];
+        }
+        return ans;
+    }    
+};
