b5b6bf31a1d1958e56f4b5d68d5fd41fae04fb65
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-09-19 14:52:02 +0530

    Time: 7 ms (22.40%) | Memory: 12.4 MB (6.93%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        
+        int res = lower_bound(nums.begin(),nums.end(),target) - nums.begin();
+        return res;
+    }
+};
