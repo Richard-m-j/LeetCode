c8455134138af08b3e76ed32ab2f495e7155453e
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-09-19 09:20:11 +0530

    Time: 0 ms (100.00%) | Memory: 13.8 MB (40.75%) - LeetSync

---
+++ 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.cpp
@@ -0,0 +1,35 @@
+class Solution {
+private:
+    vector<int> res;
+    int operastion(char op,int a,int b){
+        switch(op){
+            case '+': return a+b;
+            case '-': return a-b;
+            case '*': return a*b;
+            case '/': return a/b;
+        }
+        return 0;
+    }
+public:
+    vector<int> diffWaysToCompute(string expression) {
+        vector<int> res;
+        bool isNum = true;
+        for(int i=0;i<expression.length();i++){
+            if(!isdigit(expression[i])){
+                isNum = false;
+                // cout<<expression[i]<<" ";
+                vector<int> left = diffWaysToCompute(expression.substr(0,i));
+                vector<int> right = diffWaysToCompute(expression.substr(i+1));
+
+                for(int& x: left)
+                    for(int& y: right)
+                        res.push_back(operastion(expression[i],x,y));   
+            } 
+        }
+        if(isNum){
+            // cout<<"num";
+            res.push_back(stoi(expression));
+        }
+        return res;
+    }
+};
