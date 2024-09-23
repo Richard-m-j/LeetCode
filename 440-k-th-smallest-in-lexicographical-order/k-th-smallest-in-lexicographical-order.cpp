f861f6248c11a69b6c230b118f6a621db41fcb63
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-09-23 07:09:12 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (54.15%) - LeetSync

---
+++ 440-k-th-smallest-in-lexicographical-order/k-th-smallest-in-lexicographical-order.cpp
@@ -0,0 +1,33 @@
+class Solution {
+private:
+    int countNumbersWithPrefix(int prefix, int n) {
+        long long firstNumber = prefix, nextNumber = prefix + 1;
+        int res = 0;
+
+        while (firstNumber <= n) {
+            res += static_cast<int>(min(n + 1LL, nextNumber) - firstNumber);
+            firstNumber *= 10;
+            nextNumber *= 10;
+        }
+
+        return res;
+    }
+public:
+    int findKthNumber(int n, int k) {
+        int currentPrefix = 1;
+        --k;  
+        
+        while (k > 0) {
+            int count = countNumbersWithPrefix(currentPrefix, n);
+            if (k >= count) {
+                ++currentPrefix;  
+                k -= count;
+            } else {
+                currentPrefix *= 10;  
+                --k;
+            }
+        }
+        
+        return currentPrefix;
+    }
+};
