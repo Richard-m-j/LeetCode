751f1fcaf1fc212aa244fd82292f5e2f31bf9461
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-09-23 14:21:42 +0530

    Time: 67 ms (92.06%) | Memory: 61.1 MB (95.44%) - LeetSync

---
+++ 2755-extra-characters-in-a-string/extra-characters-in-a-string.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minExtraChar(string s, vector<string>& dictionary) {
+        int n = s.length();
+        vector<int> dp(n + 1, 0);
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i] = dp[i + 1] + 1;
+            for (auto& w : dictionary) {
+                if (i + w.length() <= n && s.substr(i, w.length()) == w){
+                    dp[i] = min(dp[i], dp[i + w.length()]);
+                    // cout<<dp[i]<<"-"<<w<<" ";
+                }
+            }
+            // cout << dp[i] << "\n";
+        }
+        return dp[0];
+    }
+};
