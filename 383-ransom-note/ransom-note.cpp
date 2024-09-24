a4e9fe744e4f65e09be14d1ecc5ff1c32beb16b2
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-09-24 22:40:12 +0530

    Time: 7 ms (97.07%) | Memory: 10.3 MB (79.61%) - LeetSync

---
+++ 383-ransom-note/ransom-note.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        vector<int> mp(26,0);
+        for(char& c: ransomNote)
+            mp[c-'a']++;
+        for(char& c: magazine)
+            mp[c-'a']--;
+        for(int i=0;i<26;i++){
+            // cout<<mp[i]<<" ";
+            if(mp[i]>0)
+                return false;
+        }
+        return true;
+    }
+};
