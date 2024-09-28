097f6993e933804c1c78b2203cb039c3ad868be0
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-09-28 10:27:59 +0530

    Time: 31 ms (66.47%) | Memory: 23.2 MB (40.33%) - LeetSync

--- 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.cpp
+++ 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.cpp
@@ -11,26 +11,22 @@ public:
         
         if (s.length() < sequenceLen) return res;
         
-        // Build the frequency map of words
         for (string &word : words) {
             wordMap[word]++;
         }
-        
-        // Try each possible starting position within a single word length window
+
         for (int i = 0; i < wordLen; ++i) {
-            int start = i;  // Sliding window start
+            int start = i;  
             unordered_map<string, int> windowMap;
             int count = 0;
             
             for (int j = i; j + wordLen <= s.length(); j += wordLen) {
                 string word = s.substr(j, wordLen);
                 
-                // If the word is in words, update the window map
-                if (wordMap.count(word)) {
+                if (wordMap.find(word)!=wordMap.end()) {
                     windowMap[word]++;
                     count++;
                     
-                    // If we exceed the count in wordMap, shrink the window
                     while (windowMap[word] > wordMap[word]) {
                         string firstWord = s.substr(start, wordLen);
                         windowMap[firstWord]--;
@@ -38,12 +34,10 @@ public:
                         count--;
                     }
                     
-                    // If the window size matches the number of words, we found a valid starting index
                     if (count == wordCount) {
                         res.push_back(start);
                     }
                 } else {
-                    // If the word is not in words, reset the window
                     windowMap.clear();
                     count = 0;
                     start = j + wordLen;
