a6a0e1be58cdffb013762ac3abf3ae3080a9829d
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-10-10 10:16:56 +0530

    Time: 0 ms (100.00%) | Memory: 18.3 MB (61.34%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-        ios::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
-        unordered_map<int, vector<int>> mp;
-        vector<int> res;
-
-        for (int i = 0; i < numbers.size(); i++) {
-            if ((mp.find(numbers[i]) != mp.end()) && !mp[numbers[i]].empty()) {
-                vector<int> it;
-                res.push_back(mp[numbers[i]][0] + 1);
-                res.push_back(i + 1);
-
-                // res.push_back(it);
-                mp[numbers[i]].pop_back();
-            } else {
-                mp[target - numbers[i]].push_back(i);
-            }
+        int n = numbers.size();
+        int l=0,r=n-1;
+        while(l<r){
+            int sum = numbers[l] + numbers[r];
+            if(sum == target)
+                return {l+1,r+1};
+            else if(sum < target)
+                l++;
+            else
+                r--;
         }
-        return res;
+        return {};
     }
 };
