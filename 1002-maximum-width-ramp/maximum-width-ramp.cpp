668637f7b1b70cf84ed610ffd8010b3ad1fe76c0
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-10-10 07:51:16 +0530

    Time: 216 ms (6.89%) | Memory: 62.1 MB (5.01%) - LeetSync

---
+++ 1002-maximum-width-ramp/maximum-width-ramp.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxWidthRamp(vector<int>& nums) {
+        map<int,int> mp;
+        int res = 0;
+        mp[nums[0]] = 0;
+        for(int j=1;j<nums.size();j++){
+            auto it = mp.upper_bound(nums[j]);
+
+            if (it != mp.begin()) {
+                --it;
+                res = max(res, j - it->second);
+                cout<<j<<" "<<it->second<<endl;
+            }
+            if(mp.begin()->first > nums[j])
+                mp[nums[j]] = j;
+        }
+        return res;
+    }
+};
