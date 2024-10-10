fdc75fc88618b4d12832eceb98d309dc6f806758
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-10-10 08:07:26 +0530

    Time: 99 ms (10.65%) | Memory: 46.8 MB (45.61%) - LeetSync

--- 1002-maximum-width-ramp/maximum-width-ramp.cpp
+++ 1002-maximum-width-ramp/maximum-width-ramp.cpp
@@ -3,19 +3,19 @@ public:
     int maxWidthRamp(vector<int>& nums) {
         ios::sync_with_stdio(0);
         cin.tie(0);
-        map<int,int> mp;
-        int res = 0;
-        mp[nums[0]] = 0;
-        for(int j=1;j<nums.size();j++){
-            auto it = mp.upper_bound(nums[j]);
-
-            if (it != mp.begin()) {
-                --it;
-                res = max(res, j - it->second);
-                // cout<<j<<" "<<it->second<<endl;
+        stack<int> s;
+        int n = nums.size();
+        int res=0;
+        for(int i=0;i<n;i++){
+            if(s.empty()||nums[s.top()]>nums[i])
+                s.push(i);
+        }
+        for(int i=n-1;i>=0;i--){
+            while(!s.empty() && nums[s.top()]<=nums[i]){
+                res = max(res,i-s.top());
+                cout<<s.top()<<" "<<i<<endl;
+                s.pop();
             }
-            if(mp.begin()->first > nums[j])
-                mp[nums[j]] = j;
         }
         return res;
     }
