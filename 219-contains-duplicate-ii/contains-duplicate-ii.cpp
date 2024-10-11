4e29416ac795fb9e38d00c09c92358fe38ebb7c2
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-10-11 13:06:37 +0530

    Time: 94 ms (98.40%) | Memory: 81.1 MB (60.40%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.cpp
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        ios::sync_with_stdio(0);
+        cin.tie(0);
         unordered_map<int,int> mp;
         for(int i=0;i<nums.size();i++){
             if(mp.count(nums[i]) && i-mp[nums[i]] <= k)
