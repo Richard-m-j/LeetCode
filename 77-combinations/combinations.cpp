dcb472f2affdfa5e0d0b4b426f9d891a0623e54e
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-10-11 22:09:20 +0530

    Time: 331 ms (6.91%) | Memory: 157.8 MB (9.22%) - LeetSync

---
+++ 77-combinations/combinations.cpp
@@ -0,0 +1,21 @@
+class Solution {
+private: 
+    vector<vector<int>> res;
+public:
+    vector<vector<int>> combine(int n, int k) {
+        vector<int> comb;
+        combinations(n,comb,1,k);
+        return res;
+    }
+    void combinations(int n,vector<int> comb, int l,int k){
+        if(comb.size()==k){
+            res.push_back(comb);
+            return;
+        }
+        for(int i=l;i<=n;i++){
+            comb.push_back(i);
+            combinations(n,comb,i+1,k);
+            comb.pop_back();
+        }
+    }
+};
