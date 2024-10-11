bddf86abc8faaaf6a0c95b0403c0ad537d493a52
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-10-11 09:22:10 +0530

    Time: 148 ms (58.08%) | Memory: 64.7 MB (40.89%) - LeetSync

---
+++ 2054-the-number-of-the-smallest-unoccupied-chair/the-number-of-the-smallest-unoccupied-chair.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int smallestChair(vector<vector<int>>& times, int targetFriend) {
+        priority_queue<pair<int, int>> heap;
+        int n = times.size();
+        for (int i=0;i<n;i++)
+            times[i].push_back(i);
+
+        sort(times.begin(), times.end(),[](auto &a,auto &b){
+            if(a[0]==b[0]) 
+                return a[1] < b[1];
+            return a[0]<b[0];
+            });
+        set<int> freeChairs;
+        for (int i = 0; i < n; i++) {
+            int arrival = times[i][0];
+            int leave = times[i][1];
+            int index = times[i][2];
+            while (!heap.empty() && arrival >= -(heap.top().first)){
+                // cout<<arrival<<" "<<-(heap.top().first)<<endl;
+                freeChairs.insert(heap.top().second);
+                heap.pop();
+            }
+            int chair;
+            if (freeChairs.size()) {
+                chair = *freeChairs.begin();
+                freeChairs.erase(chair);
+            } else {
+                chair = heap.size();
+                // cout<<heap.size()<<" ";
+            }
+            heap.push({-leave, chair});
+            if(index == targetFriend)
+                return chair;
+        }
+        return -1;
+    }
+};
