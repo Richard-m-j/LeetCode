274eefe57c80b9b5572ad9f3eae46f62643258f4
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-10-21 07:32:37 +0530

    Time: 17 ms (97.66%) | Memory: 12.8 MB (93.86%) - LeetSync

--- 1715-split-a-string-into-the-max-number-of-unique-substrings/split-a-string-into-the-max-number-of-unique-substrings.cpp
+++ 1715-split-a-string-into-the-max-number-of-unique-substrings/split-a-string-into-the-max-number-of-unique-substrings.cpp
@@ -1,30 +1,37 @@
-int speed = []{
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    return 0;
-}();
 class Solution {
-private:
-    unordered_set<string> subs;
-    int res = 0;
-    void dfs(string &s, int idx){
-        if(subs.size()>res)
-            res = subs.size();
-        if(idx>=s.length())
-            return;
-        string curr = "";
-        for(int i = idx;i<s.length();i++){
-            curr += s[i];
-            if(!subs.count(curr)){
-                subs.insert(curr);
-                dfs(s,i+1);
-                subs.erase(curr);
-            }
-        }
-    }
 public:
     int maxUniqueSplit(string s) {
-        dfs(s,0);
-        return res;
+        unordered_set<string> seen;
+        int maxCount = 0;
+        backtrack(s, 0, seen, 0, maxCount);
+        return maxCount;
+    }
+
+private:
+    void backtrack(const string& s, int start, unordered_set<string>& seen,
+                   int count, int& maxCount) {
+        // Prune: If the current count plus remaining characters can't exceed
+        // maxCount, return
+        if (count + (s.size() - start) <= maxCount) return;
+
+        // Base case: If we reach the end of the string, update maxCount
+        if (start == s.size()) {
+            maxCount = max(maxCount, count);
+            return;
+        }
+
+        // Try every possible substring starting from 'start'
+        for (int end = start + 1; end <= s.size(); ++end) {
+            string substring = s.substr(start, end - start);
+            // If the substring is unique
+            if (seen.find(substring) == seen.end()) {
+                // Add the substring to the seen set
+                seen.insert(substring);
+                // Recursively count unique substrings from the next position
+                backtrack(s, end, seen, count + 1, maxCount);
+                // Backtrack: remove the substring from the seen set
+                seen.erase(substring);
+            }
+        }
     }
 };
