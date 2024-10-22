bf15278ef55265cc8052b86e96bb60efdc653edf
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-10-22 13:31:51 +0530

    Time: 0 ms (100.00%) | Memory: 16.8 MB (80.39%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
@@ -6,23 +6,24 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 private:
-    TreeNode *ptr;
-    void helper(TreeNode* root){
-        if(!root)
+    TreeNode* ptr;
+    void helper(TreeNode* root) {
+        if (!root)
             return;
-        TreeNode *right = root->right;
+        TreeNode* right = root->right;
         ptr->right = root;
         ptr = ptr->right;
         helper(root->left);
         root->left = nullptr;
-
         helper(right);
     }
+
 public:
     void flatten(TreeNode* root) {
         TreeNode* head = new TreeNode();
