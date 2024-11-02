2ee94113a2dc84a684a2ea535ed5493ac0561016
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-11-02 06:51:42 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (83.91%) - LeetSync

--- 2580-circular-sentence/circular-sentence.cpp
+++ 2580-circular-sentence/circular-sentence.cpp
@@ -4,10 +4,9 @@ public:
         int i=0,n=sentence.length();
         char prev = sentence[n-1];
         while(i<n){
-            if(prev != sentence[i]){
-                // cout<<prev<<" "<<sentence[i]<<" "<<i<<endl;
+            if(prev != sentence[i])
                 return false;
-            }
+            
             while(i<n && sentence[i] != ' ')
                 i++;
             prev = sentence[i-1];
