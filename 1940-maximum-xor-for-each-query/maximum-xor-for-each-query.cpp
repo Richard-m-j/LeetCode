3b6249d2d65fb60c2ab2a56653a374d84c96eb70
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-11-08 06:11:09 +0530

    Time: 0 ms (100.00%) | Memory: 95.3 MB (82.32%) - LeetSync

--- 1940-maximum-xor-for-each-query/maximum-xor-for-each-query.cpp
+++ 1940-maximum-xor-for-each-query/maximum-xor-for-each-query.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     vector<int> getMaximumXor(vector<int>& nums, int maximumBit) {
         int n = nums.size();
-        vector<int> xors(n+1,0);
+        vector<int> xors(n,0);
         int maxNum = pow(2,maximumBit) - 1;
-        for(int i=0;i<n;i++){
+        xors[n-1] = nums[0];
+        for(int i=1;i<n;i++){
             xors[n-i-1] = xors[n-i] ^ nums[i];
         }
         for(int i=0;i<n;i++){
             xors[n-i-1] = xors[n-i-1] ^ maxNum;
         }
-        xors.pop_back();
         return xors;
     }
 };
