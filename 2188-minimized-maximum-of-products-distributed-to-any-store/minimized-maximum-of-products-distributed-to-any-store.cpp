982e85e2bf81eabcf20562299ef0e1586033ad57
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-11-14 09:25:09 +0530

    Time: 115 ms (27.34%) | Memory: 88.4 MB (18.48%) - LeetSync

---
+++ 2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minimizedMaximum(int n, vector<int>& quantities) {
+        sort(quantities.begin(),quantities.end());
+        int l=1,r=*max_element(quantities.begin(),quantities.end());
+        while(l<=r){
+            int m = l + (r-l)/2;
+            int numDistributions = 0;
+            for(int i=0;i<quantities.size();i++){
+                numDistributions += ceil(quantities[i]/float(m));
+            }
+            cout<<m<<" "<<numDistributions<<endl;
+            // if(numDistributions==n)
+            //     return m;
+            if(numDistributions<=n)
+                r=m-1;
+            else
+                l=m+1;
+        }
+        return l;
+    }
+};
