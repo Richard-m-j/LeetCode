04090d0bd9e72039b33ae6e655c6a7365e9559be
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-11-18 19:44:02 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (84.00%) - LeetSync

--- 1755-defuse-the-bomb/defuse-the-bomb.cpp
+++ 1755-defuse-the-bomb/defuse-the-bomb.cpp
@@ -2,18 +2,20 @@ class Solution {
 public:
     vector<int> decrypt(vector<int>& code, int k) {
         int n = code.size();
-        vector<int> res(n,0);
-        if(!k)
+        vector<int> res(n, 0);
+        if (!k)
             return res;
-        if(k>0){
-            for(int i=0;i<n;i++)
-                for(int j=(i+1)%n;j!=(i+k+1)%n;j=(j+1)%n){
-                    res[i] += code[j];}
-        }
-        else{
-            for(int i=0;i<n;i++)
-                for(int j=(i-1+n)%n;j!=(i+k-1+n)%n;j=(j-1+n)%n)
-                    res[i] += code[j];
+        if (k > 0) {
+            for (int j = 1; j != (k + 1) % n; j = (j + 1) % n)
+                res[0] += code[j];
+            for (int i = 1; i < n; i++)
+                res[i] = res[i - 1] - code[i] + code[(i + k) % n];
+
+        } else {
+            for (int j = n - 1; j != (k - 1 + n) % n; j = (j - 1 + n) % n)
+                res[0] += code[j];
+            for (int i = 1; i < n; i++)
+                res[i] = res[i - 1] + code[i - 1] - code[(i + k - 1 + n) % n];
         }
         return res;
     }
