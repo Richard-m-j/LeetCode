3c9110bfd5624d7c1b72d00a99b45eb2b7d151d9
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-11-21 09:44:41 +0530

    Time: 48 ms (88.66%) | Memory: 172.1 MB (84.31%) - LeetSync

--- 2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.cpp
+++ 2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.cpp
@@ -4,6 +4,7 @@ public:
         ios::sync_with_stdio(0);
         cin.tie(0);
         vector<vector<int>> mat(m,vector<int>(n,0));
+        vector<pair<int,int>> dirs = {{0,-1},{0,1},{1,0},{-1,0}};
         //0 empty
         //1 guarded
         //2 wall
@@ -15,7 +16,6 @@ public:
             mat[guard[0]][guard[1]]=3;
         int count=0;
         for(auto& guard: guards){
-            vector<pair<int,int>> dirs = {{0,-1},{0,1},{1,0},{-1,0}};
             for(auto& dir: dirs){
                 int i=guard[0] + dir.first;
                 int j=guard[1] + dir.second;
@@ -31,11 +31,6 @@ public:
                 }
             }
         }
-        // int res = 0;
-        // for(int i=0;i<m;i++)
-        //     for(int j=0;j<n;j++)
-        //         if(mat[i][j] == 0)
-        //             res++;
         return m*n - walls.size() - guards.size() - count;
     }
 };
