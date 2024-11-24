b9ae524ada25349bb5183d7e20ad4b759763581f
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-11-24 09:13:43 +0530

    Time: 0 ms (100.00%) | Memory: 38.5 MB (30.74%) - LeetSync

---
+++ 2089-maximum-matrix-sum/maximum-matrix-sum.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    long long maxMatrixSum(vector<vector<int>>& matrix) {
+        long long res = 0;
+        int n = matrix.size();
+        int countCol = 0;
+        int x = 0, y = 0;
+        for (int i = 0; i < n; i++) {
+            int minIdx = 0;
+            int countRow = 0;
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] < 0) {
+                    matrix[i][j] *= -1;
+                    countRow++;
+                }
+                if (matrix[i][j] < matrix[x][y]){
+                    x = i;
+                    y = j;
+                }
+                res += matrix[i][j];
+            }
+            if (countRow % 2)
+                countCol++;
+        }
+        //14 13 7
+        // cout<<res<<" ";
+        if (countCol % 2) {
+            res -= matrix[x][y] * 2;
+        }
+        return res;
+    }
+};
