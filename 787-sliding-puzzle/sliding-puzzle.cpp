c81060cd43a9917dc3dc575ba36ae4b2e6065842
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-11-25 10:49:14 +0530

    Time: 15 ms (34.54%) | Memory: 16.1 MB (21.03%) - LeetSync

--- 787-sliding-puzzle/sliding-puzzle.cpp
+++ 787-sliding-puzzle/sliding-puzzle.cpp
@@ -1,3 +1,8 @@
+int speedUp=[]{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    return 0;
+}();
 class Solution {
 private:
     vector<vector<int>> goalPos = {{1, 2}, {0, 0}, {0, 1}, {0, 2}, {1, 0}, {1, 1}};
