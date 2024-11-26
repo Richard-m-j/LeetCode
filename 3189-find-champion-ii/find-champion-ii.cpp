72cecdb0057cf57c336851a6de8f844198eee133
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-11-26 06:10:41 +0530

    Time: 4 ms (72.43%) | Memory: 92.9 MB (98.48%) - LeetSync

--- 3189-find-champion-ii/find-champion-ii.cpp
+++ 3189-find-champion-ii/find-champion-ii.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
     int findChampion(int n, vector<vector<int>>& edges) {
-        ios::sync_with_stdio(0);
-        cin.tie(0);
-        vector<int> indegree(n,0);
-        for(auto edge: edges)
+        vector<int> indegree(n, 0);
+
+        // Calculate indegree for each team
+        for (const vector<int>& edge : edges) {
             indegree[edge[1]]++;
-        int champion=-1;
-        for(int i=0;i<n;i++)
-            if(indegree[i]==0){
-                if(champion==-1)
-                    champion=i;
-                else
+        }
+
+        int champion = -1; // Start with no champion identified
+
+        // Find the team with zero indegree
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] == 0) {
+                if (champion != -1) {
+                    // More than one team has zero indegree, no single champion
                     return -1;
+                }
+                champion = i; // Identify the potential champion
             }
-                
+        }
+
         return champion;
     }
 };
