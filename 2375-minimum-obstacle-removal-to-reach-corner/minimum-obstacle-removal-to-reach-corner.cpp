5666eadd1f2d0147d01fcee1803845786755810b
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-11-28 17:51:43 +0530

    Time: 400 ms (45.68%) | Memory: 166 MB (42.91%) - LeetSync

--- 2375-minimum-obstacle-removal-to-reach-corner/minimum-obstacle-removal-to-reach-corner.cpp
+++ 2375-minimum-obstacle-removal-to-reach-corner/minimum-obstacle-removal-to-reach-corner.cpp
@@ -1,35 +1,35 @@
 class Solution {
+private:
+    vector<pair<int, int>> dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
 public:
-    int minimumObstacles(vector<vector<int>>& mat) {
-         vector<pair<int,int>>d={{-1,0},{0,1},{1,0},{0,-1}};
-        int n=mat.size();
-        int m=mat[0].size();
-        queue<pair<int,int>>q;
-        q.push({0,0});
-        vector<vector<int>>dp(n,vector<int>(m,INT_MAX));
-        dp[0][0]=0;
-        while(q.size())
-        {
-            int size=q.size();
-            for(int i=0;i<size;i++)
-            {
-                auto temp=q.front();
+    int minimumObstacles(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+
+        vector<vector<int>> dp(n, vector<int>(m, INT_MAX));
+
+        queue<pair<int, int>> q;
+        q.push({0, 0});
+        dp[0][0] = 0;
+        while (q.size()) {
+            int qLen = q.size();
+            for (int i = 0; i < qLen; i++) {
+                auto node = q.front();
                 q.pop();
-                int x=temp.first;
-                int y=temp.second;
-                for(auto it:d)
-                {
-                    int nx=x+it.first;
-                    int ny=y+it.second;
-                    if(nx>=0&&ny>=0&&nx<n&&ny<m&&dp[x][y]+mat[nx][ny]<dp[nx][ny])
-                    {
-                        dp[nx][ny]=dp[x][y]+mat[nx][ny];
-                         q.push({nx,ny});
+                int x = node.first;
+                int y = node.second;
+                for (auto& d : dirs) {
+                    int nx = x + d.first;
+                    int ny = y + d.second;
+                    if (nx >= 0 && ny >= 0 && nx < n && ny < m &&
+                        dp[x][y] + grid[nx][ny] <dp[nx][ny]) {
+                        dp[nx][ny] = dp[x][y] + grid[nx][ny];
+                        q.push({nx, ny});
                     }
                 }
             }
         }
-        return dp[n-1][m-1];
-                
+        return dp[n - 1][m - 1];
     }
 };
