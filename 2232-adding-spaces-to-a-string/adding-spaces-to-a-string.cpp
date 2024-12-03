bd5683936616a4d7e4959223f0c0f3aeecc75ef1
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-12-03 06:35:35 +0530

    Time: 23 ms (37.65%) | Memory: 84.6 MB (27.25%) - LeetSync

--- 2232-adding-spaces-to-a-string/adding-spaces-to-a-string.cpp
+++ 2232-adding-spaces-to-a-string/adding-spaces-to-a-string.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     string addSpaces(string s, vector<int>& spaces) {
+        ios::sync_with_stdio(0);
+        cin.tie(0);
         int i=0,j=0;
         string res="";
         while(i<s.length()){
