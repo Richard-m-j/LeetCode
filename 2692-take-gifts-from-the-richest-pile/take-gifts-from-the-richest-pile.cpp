cf4f8a5c51764003e860d1335f00ac71d17b1956
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-12-12 07:02:51 +0530

    Time: 7 ms (14.34%) | Memory: 13.1 MB (78.35%) - LeetSync

---
+++ 2692-take-gifts-from-the-richest-pile/take-gifts-from-the-richest-pile.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    long long pickGifts(vector<int>& gifts, int k) {
+        priority_queue<int> pq;
+        for(int& gift: gifts)
+            pq.push(gift);
+        while(k && !pq.empty()){
+            int g = pq.top();
+            pq.pop();
+            int sqroot = sqrt(g);
+            pq.push(sqroot);
+            k--;
+        }
+        long long res=0;
+        while(!pq.empty()){
+            int g = pq.top();
+            pq.pop();
+            res += g;
+        }
+        return res;
+    }
+};
