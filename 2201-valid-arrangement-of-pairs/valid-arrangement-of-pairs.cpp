83eb818553a81563c863ea08f44a2d3299183506
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-12-14 14:32:02 +0530

    Time: 299 ms (62.31%) | Memory: 263.3 MB (58.71%) - LeetSync

--- 2201-valid-arrangement-of-pairs/valid-arrangement-of-pairs.cpp
+++ 2201-valid-arrangement-of-pairs/valid-arrangement-of-pairs.cpp
@@ -1,49 +1,40 @@
 class Solution {
 public:
     vector<vector<int>> validArrangement(vector<vector<int>>& pairs) {
-        unordered_map<int, vector<int>> adjacencyList;
-        unordered_map<int, int> inOutDegree;
-        
-        // Build graph and count in/out degrees
-        for (const auto& pair : pairs) {
-            adjacencyList[pair[0]].push_back(pair[1]);
-            inOutDegree[pair[0]]++;  // out-degree
-            inOutDegree[pair[1]]--;  // in-degree
+        unordered_map<int,vector<int>> graph;
+        unordered_map<int,int> degree;
+        for(vector<int>& node: pairs){
+            graph[node[0]].push_back(node[1]);
+            degree[node[0]]++;
+            degree[node[1]]--;
         }
-        
-        // Find starting node
-        int startNode = pairs[0][0];
-        for (const auto& [node, degree] : inOutDegree) {
-            if (degree == 1) {
-                startNode = node;
+        int start=pairs[0][0];
+        for(auto& it: degree)
+            if(it.second==1){
+                start = it.first;
                 break;
-            }
-        }
-        
-        vector<int> path;
+            } 
+        vector<int> path;  
         stack<int> nodeStack;
-        nodeStack.push(startNode);
-        
-        while (!nodeStack.empty()) {
-            auto& neighbors = adjacencyList[nodeStack.top()];
-            if (neighbors.empty()) {
-                path.push_back(nodeStack.top());
+        nodeStack.push(start);
+        while(!nodeStack.empty()){
+            int node = nodeStack.top();
+            vector<int>& neighbours = graph[node];
+            if(neighbours.empty()){
+                path.push_back(node);
                 nodeStack.pop();
-            } else {
-                int nextNode = neighbors.back();
-                nodeStack.push(nextNode);
-                neighbors.pop_back();
+            }
+            else{
+                int next = neighbours.back();
+                nodeStack.push(next);
+                neighbours.pop_back();
             }
         }
-        
-        vector<vector<int>> arrangement;
-        int pathSize = path.size();
-        arrangement.reserve(pathSize - 1);
-        
-        for (int i = pathSize - 1; i > 0; --i) {
-            arrangement.push_back({path[i], path[i-1]});
+        vector<vector<int>> res;
+        for(int i=path.size()-1;i>0;i--){
+            vector<int> pair = {path[i],path[i-1]};
+            res.push_back(pair);
         }
-        
-        return arrangement;
+        return res;
     }
 };
