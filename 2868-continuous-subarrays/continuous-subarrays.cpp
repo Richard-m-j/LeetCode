36fe7895a0fd38b88fb4f49c8537c7adb885a679
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2024-12-14 11:04:10 +0530

    Time: 51 ms (100.00%) | Memory: 121.4 MB (53.50%) - LeetSync

--- 2868-continuous-subarrays/continuous-subarrays.cpp
+++ 2868-continuous-subarrays/continuous-subarrays.cpp
@@ -5,28 +5,28 @@ public:
         cin.tie(0);
         long long res = 0;
         deque<int> mins, maxs;
-        int n = nums.size(), left = 0;
+        int n = nums.size(), l = 0;
 
-        for (int right = 0; right < n; ++right) {
+        for (int r = 0; r < n; ++r) {
             // Maintain increasing order in mins for the current window
-            while (!mins.empty() && nums[right] < nums[mins.back()])
+            while (!mins.empty() && nums[r] < nums[mins.back()])
                 mins.pop_back();
-            mins.push_back(right);
+            mins.push_back(r);
 
             // Maintain decreasing order in maxs for the current window
-            while (!maxs.empty() && nums[right] > nums[maxs.back()])
+            while (!maxs.empty() && nums[r] > nums[maxs.back()])
                 maxs.pop_back();
-            maxs.push_back(right);
+            maxs.push_back(r);
 
             // Shrink window if condition is violated
             while (nums[maxs.front()] - nums[mins.front()] > 2) {
-                if (mins.front() == left) mins.pop_front();
-                if (maxs.front() == left) maxs.pop_front();
-                ++left;
+                if (mins.front() == l) mins.pop_front();
+                if (maxs.front() == l) maxs.pop_front();
+                ++l;
             }
 
-            // For a valid window, all subarrays ending at 'right' are valid
-            res += (right - left + 1);
+            // For a valid window, all subarrays ending at 'r' are valid
+            res += (r - l + 1);
         }
 
         return res;
