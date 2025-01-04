1e9cd324312891c3bea3a8fb67972ebd58f1efe3
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-01-04 20:12:53 +0530

    Time: 131 ms (90.19%) | Memory: 16 MB (47.85%) - LeetSync

---
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int countPalindromicSubsequence(string s) {
+        vector<int> firstIndex(26,-1);
+        vector<int> lastIndex(26,-1);
+
+        for(int i=0;i<s.length();i++){
+            if(firstIndex[s[i]-'a']==-1)
+                firstIndex[s[i]-'a'] = i;
+            lastIndex[s[i]-'a'] = max(lastIndex[s[i]-'a'],i);            
+        }
+        
+        int res=0;
+        for(int i=0;i<26;i++){
+            if(firstIndex[i] == -1 || lastIndex[i]==-1)
+                continue;
+            unordered_set<char> uniques;
+            for(int j=firstIndex[i]+1;j<lastIndex[i];j++)
+                if(!uniques.count(s[j]))
+                    uniques.insert(s[j]);
+            res += uniques.size();
+        }
+        return res;
+    }
+};
