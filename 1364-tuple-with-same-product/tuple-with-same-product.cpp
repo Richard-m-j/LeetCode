7ce96a6e5f8fa3c18cdbbd7c2f3f3a8b77c1158a
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-02-06 08:37:22 +0530

    Time: 329 ms (42.32%) | Memory: 84.9 MB (67.71%) - LeetSync

--- 1364-tuple-with-same-product/tuple-with-same-product.cpp
+++ 1364-tuple-with-same-product/tuple-with-same-product.cpp
@@ -3,7 +3,7 @@ public:
     int tupleSameProduct(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         unordered_map<int,int> mp;
-        for(int i=0;i<nums.size();i++){
+        for(int i=0;i<nums.size()-2;i++){
             for(int j=i+1;j<nums.size();j++){
                 mp[nums[i]*nums[j]]++;
             }
