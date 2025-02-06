3074269f53d65a1594e156f9809381a9c25030c0
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-02-06 08:54:06 +0530

    Time: 288 ms (86.21%) | Memory: 85.4 MB (19.44%) - LeetSync

--- 1364-tuple-with-same-product/tuple-with-same-product.cpp
+++ 1364-tuple-with-same-product/tuple-with-same-product.cpp
@@ -1,17 +1,19 @@
+static auto init = []() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+  return nullptr;
+}();
 class Solution {
 public:
     int tupleSameProduct(vector<int>& nums) {
-        ios::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
-        
         // sort(nums.begin(),nums.end());
         unordered_map<int,int> mp;
         int res = 0;
         for(int i=0;i<nums.size();i++)
-            for(int j=i+1;j<nums.size();j++){
+            for(int j=i+1;j<nums.size();j++)
                 res += (mp[nums[i]*nums[j]]++)*8;
-            }
+            
             
         
         // int res=0;
