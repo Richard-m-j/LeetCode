77d4182bd4adccba64b47335f28df8879bbe4ced
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-02-13 16:10:09 +0530

    Time: 286 ms (5.04%) | Memory: 131.5 MB (16.98%) - LeetSync

---
+++ 3332-minimum-operations-to-exceed-threshold-value-ii/minimum-operations-to-exceed-threshold-value-ii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        map<long long,int> mp;
+        for(int& num: nums)
+            mp[num]++;
+        int res=0;
+        while(true){
+            long long x,y;
+            auto it = mp.begin();
+            x=it->first;
+            if(it->second>1)
+                y=it->first;
+            else
+                y=(++it)->first;
+            // cout<<x<<" "<<y<<endl;
+            mp[x]--;
+            if(!mp[x])
+                mp.erase(x);
+            mp[y]--;
+            if(!mp[y])
+                mp.erase(y);
+            if(x>=k)
+                break;
+            long long xy = (long long)min(x,y)*(2)+max(x,y);
+            mp[xy]++;
+            res++;
+        }
+        return res;
+    }
+};
