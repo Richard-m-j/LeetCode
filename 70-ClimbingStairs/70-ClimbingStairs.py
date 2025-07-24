d76d7bf748db306cf53a87b54d4c2ceae20b6e7b
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:35:17 +0530

    Create 70-ClimbingStairs

---
+++ 70-ClimbingStairs/70-ClimbingStairs.py
@@ -0,0 +1,16 @@
+# Last updated: 7/24/2025, 8:35:18 AM
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 0:
+            return 0
+        elif n == 1:
+            return 1
+
+        fib_0 = 0
+        fib_1 = 1
+
+        for i in range(2, n + 2):
+            fib_n = fib_0 + fib_1
+            fib_0, fib_1 = fib_1, fib_n
+
+        return fib_1
