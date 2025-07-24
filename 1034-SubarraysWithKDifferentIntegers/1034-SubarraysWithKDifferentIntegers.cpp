1083d32a01c04ec881875ae4917accbbedf9bbaf
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:28:15 +0530

    Create 1034-SubarraysWithKDifferentIntegers

---
+++ 1034-SubarraysWithKDifferentIntegers/1034-SubarraysWithKDifferentIntegers.cpp
@@ -0,0 +1,22 @@
+// Last updated: 7/24/2025, 8:28:15 AM
+class Solution {
+public:
+    int subarraysWithKDistinct(vector<int>& nums, int k) {
+        return atMost(nums,k) - atMost(nums,k-1);
+    }
+    int atMost(vector<int>& nums, int k) {
+        unordered_map<int, int> mp;
+        int ans = 0, l = 0;
+        for (int r = 0; r < nums.size(); r++) {
+            mp[nums[r]]++;
+
+            while (l <= r && mp.size() > k) {
+                if (--mp[nums[l]] == 0)
+                    mp.erase(nums[l]);
+                l++;
+            }
+            ans += r - l + 1;
+        }
+        return ans;
+    }
+};
