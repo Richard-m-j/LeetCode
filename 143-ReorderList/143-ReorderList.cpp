a7a850c214e1b03ded213635dc6f81bf9d7c4c3d
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:33:41 +0530

    Create 143-ReorderList

---
+++ 143-ReorderList/143-ReorderList.cpp
@@ -0,0 +1,39 @@
+// Last updated: 7/24/2025, 8:33:42 AM
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+        vector<ListNode*> nums;
+        ListNode* ptr = head;
+        while (ptr) {
+            nums.push_back(ptr);
+            ptr = ptr->next;
+        }
+        int n = nums.size();
+        if (n == 1 || n == 2)
+            return;
+        ptr = head;
+        head->next = nums[n - 1];
+        ptr = ptr->next;
+        int l = n % 2 ? n / 2 + 1 : n / 2;
+        for (int i = 1; i < l; i++) {
+            ptr->next = nums[i];
+            ptr = ptr->next;
+
+            ptr->next = nums[n - i - 1];
+            ptr = ptr->next;
+            cout << nums[i]->val << " " << nums[n - i - 1]->val << " ";
+        }
+        ptr->next = NULL;
+        return;
+    }
+};
