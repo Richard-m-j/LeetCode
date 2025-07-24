e6e125d51860d9b927bb65c267bde18271319a60
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:25:35 +0530

    Create 2006-FindTheStudentThatWillReplaceTheChalk

---
+++ 2006-FindTheStudentThatWillReplaceTheChalk/2006-FindTheStudentThatWillReplaceTheChalk.cpp
@@ -0,0 +1,27 @@
+// Last updated: 7/24/2025, 8:25:35 AM
+class Solution {
+public:
+    int chalkReplacer(vector<int>& chalk, int k) {
+        int n = chalk.size();
+
+        long sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += chalk[i];
+            if (sum > k) {
+                for (int j = 0; j <= i; j++) {
+                    k -= chalk[j];
+                    if (k < 0)
+                        return j;
+                }
+            }
+        }
+
+        int remainingChalk = k % sum;
+        for (int i = 0; i < n; i++) {
+            remainingChalk -= chalk[i];
+            if (remainingChalk < 0)
+                return i;
+        }
+        return 0;
+    }
+};
