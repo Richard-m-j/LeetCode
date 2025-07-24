df30ff42f1f8384cf923e8c21d6e93070be9d001
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:25:57 +0530

    Create 1802-NumberOfStudentsUnableToEatLunch

---
+++ 1802-NumberOfStudentsUnableToEatLunch/1802-NumberOfStudentsUnableToEatLunch.cpp
@@ -0,0 +1,12 @@
+// Last updated: 7/24/2025, 8:25:57 AM
+class Solution {
+public:
+        int countStudents(vector<int>& A, vector<int>& B) {
+        int count[] = {0, 0}, n = A.size(), k;
+        for (int a : A)
+            count[a]++;
+        for (k = 0; k < n && count[B[k]] > 0; ++k)
+            count[B[k]]--;
+        return n - k;
+    }
+};
