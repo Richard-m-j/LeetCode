0c8618f923dd2c72e4e91f4e20fcb73658b1e199
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:24:24 +0530

    Create 2414-MovePiecesToObtainAString

---
+++ 2414-MovePiecesToObtainAString/2414-MovePiecesToObtainAString.cpp
@@ -0,0 +1,30 @@
+// Last updated: 7/24/2025, 8:24:24 AM
+class Solution {
+public:
+    bool canChange(string st, string tar) {
+        int n=tar.length();
+        int i=0,j=0;
+        while(i<=n && j<=n){
+            
+            while(i<n && tar[i]=='_') i++;
+            while(j<n && st[j]=='_') j++;
+            
+            if(i==n || j==n){
+                return i==n && j==n;
+            }
+            
+            if(tar[i]!=st[j]) return false;
+            
+            if(tar[i]=='L'){
+                if(j<i) return false;
+            }
+            else{
+                if(i<j) return false;
+            }
+            
+            i++;
+            j++;
+        }
+        return true;
+    }
+};
