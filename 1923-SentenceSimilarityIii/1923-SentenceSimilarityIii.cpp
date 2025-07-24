98506fa97c86e966940ada0070c72a43fcdc484d
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:25:40 +0530

    Create 1923-SentenceSimilarityIii

---
+++ 1923-SentenceSimilarityIii/1923-SentenceSimilarityIii.cpp
@@ -0,0 +1,33 @@
+// Last updated: 7/24/2025, 8:25:41 AM
+class Solution {
+public:
+    bool areSentencesSimilar(string s1, string s2) {
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+        string temp = "";
+        deque<string> a, b;
+        for (char c : s1) {
+            if (c == ' ')
+                a.push_back(temp), temp = "";
+            else
+                temp += c;
+        }
+        a.push_back(temp), temp = "";
+        for (char c : s2) {
+            if (c == ' ')
+                b.push_back(temp), temp = "";
+            else
+                temp += c;
+        }
+        b.push_back(temp), temp = "";
+
+        while (a.size() && b.size() && (a.front() == b.front()))
+            a.pop_front(), b.pop_front();
+
+        while (a.size() && b.size() && (a.back() == b.back()))
+            a.pop_back(), b.pop_back();
+        if (!a.size() || !b.size())
+            return true;
+        return false;
+    }
+};
