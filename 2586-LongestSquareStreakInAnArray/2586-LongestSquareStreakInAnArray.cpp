18e6657c3bc85d254186ef49a76ecbc45711a271
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:23:47 +0530

    Create 2586-LongestSquareStreakInAnArray

---
+++ 2586-LongestSquareStreakInAnArray/2586-LongestSquareStreakInAnArray.cpp
@@ -0,0 +1,27 @@
+// Last updated: 7/24/2025, 8:23:47 AM
+class Solution {
+public:
+    int longestSquareStreak(vector<int>& nums) {
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+        sort(nums.begin(),nums.end());
+        unordered_set<int> s;
+        for(int & num: nums)
+            s.insert(num);
+        int res = 0;
+        for(int i=0;i<nums.size();i++){
+            if(s.count(nums[i])){
+                long long square = nums[i];
+                int len = 0;
+                while(s.count(square)){
+                    // cout<<square<<" ";
+                    s.erase(square);
+                    square *= square;
+                    len++;
+                    res = max(res,len);
+                }
+            }
+        }
+        return res>1?res:-1;
+    }
+};
