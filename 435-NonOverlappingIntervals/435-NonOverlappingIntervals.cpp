bb40a23bd1939f710bd570a6c652d9aea5edba5f
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:30:58 +0530

    Create 435-NonOverlappingIntervals

---
+++ 435-NonOverlappingIntervals/435-NonOverlappingIntervals.cpp
@@ -0,0 +1,30 @@
+// Last updated: 7/24/2025, 8:30:58 AM
+class Solution {
+public:
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+        // sort the intervals
+        sort(intervals.begin(), intervals.end(),
+             [](const auto& a, const auto& b) {
+                 if (a[0] != b[0])
+                     return a[0] < b[0];
+                 return a[1] < b[1];
+             });
+        int res = 0;
+        int l = intervals[0][0];
+        int r = intervals[0][1];
+
+        for (int i = 1; i < intervals.size(); i++) {
+            if (intervals[i][0] == l || intervals[i][0] < r) {
+                r = min(r, intervals[i][1]);
+                res++;
+            } else {
+                l = intervals[i][0];
+                r = intervals[i][1];
+            }
+        }
+        return res;
+    }
+};
