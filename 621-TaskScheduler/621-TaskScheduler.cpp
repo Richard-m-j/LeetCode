8eaea5e319e6c81b6ec1f7844d5c4dde8ec13cca
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:30:05 +0530

    Create 621-TaskScheduler

---
+++ 621-TaskScheduler/621-TaskScheduler.cpp
@@ -0,0 +1,38 @@
+// Last updated: 7/24/2025, 8:30:05 AM
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        ios::sync_with_stdio(false);
+        cin.tie(0);
+        cout.tie(0);
+        priority_queue<pair<int,char>> q;
+        unordered_map<char, int> mp;
+        for (char& task : tasks)
+            mp[task-'A']++;
+        for (auto& it : mp)
+            q.push({it.second, it.first});
+        int res = 0;
+        while (!q.empty()) {
+            priority_queue<pair<int,char>> q1;
+            int count = min((int)q.size(), n+1);
+            int delta = 0;
+            for (int i = 0; i < count; i++) {
+                auto it = q.top();
+                q.pop();
+                delta++;
+                if(it.first>1)
+                    q1.push({it.first - 1, it.second});
+            }
+            while (!q.empty()) {
+                q1.push(q.top());
+                q.pop();
+            }
+            if(!q1.empty())
+                res += delta <= n ? n + 1 : delta;
+            else
+                res += delta;
+            q = q1;
+        }
+        return res;
+    }
+};
