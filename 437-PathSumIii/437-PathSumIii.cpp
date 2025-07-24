774e9632ff0e141a7bfc632f9556ab15cd144869
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:30:55 +0530

    Create 437-PathSumIii

---
+++ 437-PathSumIii/437-PathSumIii.cpp
@@ -0,0 +1,40 @@
+// Last updated: 7/24/2025, 8:30:55 AM
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int pathSum(TreeNode* root, int targetSum) {
+        hash[0] = 1;
+        recCount(root,targetSum,0);
+        return count;
+
+    }
+    void recCount(TreeNode* root, int targetSum, long pathSum )
+    {
+        if(!root)
+            return;
+        
+        long currentSum = pathSum + root->val;
+        long x = currentSum - targetSum;
+
+        if(hash.find(x) != hash.end())
+            count+= hash[x];
+        hash[currentSum]++;
+
+        recCount(root->left,targetSum,currentSum);
+        recCount(root->right,targetSum,currentSum);
+        hash[currentSum]--;        
+    }
+private:
+    int count = 0;
+    unordered_map<long,int> hash;
+};
