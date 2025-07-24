8b81f607577175c6d3419e8012fae48fc515e592
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:29:39 +0530

    Create 714-BestTimeToBuyAndSellStockWithTransactionFee

---
+++ 714-BestTimeToBuyAndSellStockWithTransactionFee/714-BestTimeToBuyAndSellStockWithTransactionFee.cpp
@@ -0,0 +1,16 @@
+// Last updated: 7/24/2025, 8:29:39 AM
+class Solution {
+public:
+    int maxProfit(vector<int>& prices, int fee) {
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+        int buy = prices[0];
+        int sell = 0;
+        for(int i=1;i<prices.size();i++){
+            sell = max(sell,prices[i]-buy-fee);
+
+            buy = min(buy,prices[i]-sell);
+        }
+        return sell;
+    }
+};
