a7d130d9f4a37db8619c44509f91f281d72db62f
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:36:48 +0530

    Create 6-ZigzagConversion

---
+++ 6-ZigzagConversion/6-ZigzagConversion.cpp
@@ -0,0 +1,32 @@
+// Last updated: 7/24/2025, 8:36:49 AM
+static const int speedUp = [] {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    return 0;
+}();
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        if (numRows == 1 || s.length() <= numRows)
+            return s;
+
+        vector<string> rows(numRows);
+        int rowIndex = 0;
+        bool goingDown = false;
+
+        for (char& c : s) {
+            rows[rowIndex] += c;
+
+            if (rowIndex == 0 || rowIndex == numRows - 1)
+                goingDown = !goingDown;
+            rowIndex += goingDown ? 1 : -1;
+        }
+
+        string result ="";
+        for (string& row : rows) {
+            result += row;
+        }
+
+        return result;
+    }
+};
