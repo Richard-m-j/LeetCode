01ddbf7da5e05bced6ec5eb4432c9225d0b5a58a
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:35:10 +0530

    Create 72-EditDistance

---
+++ 72-EditDistance/72-EditDistance.cpp
@@ -0,0 +1,31 @@
+// Last updated: 7/24/2025, 8:35:10 AM
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+        int m = word1.size();
+        int n = word2.size();
+        
+        vector<vector<int>> dp(m+1,vector<int>(n+1,0));
+        
+        for(int i=1;i<=m;i++)
+            dp[i][0] = i;
+        for(int i=1;i<=n;i++)
+            dp[0][i] = i;
+
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                // cout<<word1[i-1]<<" "<<word2[j-1]<<endl;
+                if(word1[i-1]==word2[j-1]){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = min({dp[i-1][j],dp[i][j-1],dp[i-1][j-1]})+1;
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+};
