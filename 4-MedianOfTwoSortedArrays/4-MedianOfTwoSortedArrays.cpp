ae97aa8c335b223863338698193e54c97e03bc60
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:36:51 +0530

    Create 4-MedianOfTwoSortedArrays

---
+++ 4-MedianOfTwoSortedArrays/4-MedianOfTwoSortedArrays.cpp
@@ -0,0 +1,31 @@
+// Last updated: 7/24/2025, 8:36:52 AM
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+
+        int i=0,j=0;
+        int m = (n1+n2)/2;
+        int m1=0,m2=0;
+        while(i+j<=m){
+            m2 = m1;
+            if(i<n1 && j<n2){
+                if(nums1[i]>nums2[j])
+                    m1 = nums2[j++];
+                else
+                    m1=nums1[i++];
+            }
+            else{
+                if(i<n1)
+                    m1=nums1[i++];
+                else
+                    m1=nums2[j++];
+            }
+        }
+        if((n1+n2)%2 == 1){
+            return static_cast<double>(m1);
+        }
+        return (static_cast<double>(m1)+static_cast<double>(m2))/2;
+    }
+};
