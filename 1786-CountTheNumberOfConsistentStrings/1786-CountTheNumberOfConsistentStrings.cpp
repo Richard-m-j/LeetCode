b5ee905a566a663d842823fece2963f9da8a908c
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:26:01 +0530

    Create 1786-CountTheNumberOfConsistentStrings

---
+++ 1786-CountTheNumberOfConsistentStrings/1786-CountTheNumberOfConsistentStrings.cpp
@@ -0,0 +1,24 @@
+// Last updated: 7/24/2025, 8:26:02 AM
+class Solution {
+public:
+    int countConsistentStrings(string allowed, vector<string>& words) {
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+        int res=0;
+        vector<int> mp(26,0);
+        for(char& c:allowed)
+            mp[c-'a']++;
+        for(auto& word: words){
+            bool consistent = true;
+            for(char&c:word){
+                if(!mp[c-'a']){
+                    consistent = false;
+                    break;
+                }
+            }
+            if(consistent)
+                res++;
+        }
+        return res;
+    }
+};
