716d0a6e80b55a38f5180fa0668452b56fb43a09
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:26:33 +0530

    Create 1576-ReorderRoutesToMakeAllPathsLeadToTheCityZero

---
+++ 1576-ReorderRoutesToMakeAllPathsLeadToTheCityZero/1576-ReorderRoutesToMakeAllPathsLeadToTheCityZero.cpp
@@ -0,0 +1,44 @@
+// Last updated: 7/24/2025, 8:26:34 AM
+class Solution {
+public:
+    int minReorder(int n, vector<vector<int>>& connections) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+        vector <vector <pair <int, bool>>> graph(n);
+
+        // Build the graph with direction information
+        for (const auto& con : connections) 
+        {
+            graph[con[0]].emplace_back(con[1], true);   // Forward edge
+            graph[con[1]].emplace_back(con[0], false);  // Reverse edge
+        }
+        int ans = 0;
+        vector<bool> visited(n, false);
+        stack<int> stack;
+        stack.push(0);
+
+        while (!stack.empty()) 
+        {
+            int node = stack.top();
+            stack.pop();
+            visited[node] = true;
+
+            for (const auto& neighbor : graph[node]) 
+            {
+                int nextNode = neighbor.first;
+                bool isForwardEdge = neighbor.second;
+
+                if (!visited[nextNode]) 
+                {
+                    if (isForwardEdge) 
+                        ans++;  // Reorder needed for this edge
+                    
+                    stack.push(nextNode);
+                }
+            }
+        }
+
+        return ans;
+    }
+};
