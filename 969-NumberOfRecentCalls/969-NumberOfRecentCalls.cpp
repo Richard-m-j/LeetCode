eca1b0cb62427d10ad54b89cdad7f77d5d49feb2
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:28:33 +0530

    Create 969-NumberOfRecentCalls

---
+++ 969-NumberOfRecentCalls/969-NumberOfRecentCalls.cpp
@@ -0,0 +1,26 @@
+// Last updated: 7/24/2025, 8:28:33 AM
+class RecentCounter {
+public:
+    RecentCounter() {
+        
+    }
+    
+    int ping(int t) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
+
+        requests.push(t);
+        while(requests.front()<t-3000)
+            requests.pop();
+        return requests.size();
+    }
+private:
+    queue <int> requests;
+};
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter* obj = new RecentCounter();
+ * int param_1 = obj->ping(t);
+ */
