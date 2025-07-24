ffa06cb4b24c8f29283ecfd0a9695b3eac4e0bb3
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:24:03 +0530

    Create 2527-CountSubarraysWithFixedBounds

---
+++ 2527-CountSubarraysWithFixedBounds/2527-CountSubarraysWithFixedBounds.cpp
@@ -0,0 +1,20 @@
+// Last updated: 7/24/2025, 8:24:04 AM
+class Solution {
+public:
+    long long countSubarrays(vector<int>& nums, int minK, int maxK) {
+        int l=0,r=0,mini=-1,maxi=-1;
+        long long ans = 0;
+        for(;r<nums.size();r++){
+            if(nums[r]<minK || nums[r]>maxK){
+                mini = maxi = -1;
+                l = r+1;
+            }
+            if(nums[r] == minK)
+                mini = r;
+            if(nums[r] == maxK)
+                maxi = r;
+            ans += max(0,min(maxi,mini) - l + 1);
+        }
+        return ans;
+    }
+};
