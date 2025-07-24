bd8d1444f7c7ba4ced2a834e40be2742dcd82c82
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:35:49 +0530

    Create 40-CombinationSumIi

---
+++ 40-CombinationSumIi/40-CombinationSumIi.cpp
@@ -0,0 +1,32 @@
+// Last updated: 7/24/2025, 8:35:49 AM
+static const auto speedUp = [](){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    return 0;
+}();
+class Solution {
+private:
+    void helper(vector<int>& candidates, int target,vector<vector<int>>& combinations,vector<int>& current, int n){
+        if(target == 0){
+            combinations.push_back(current);
+            return;
+        }
+        for(int i=n;i<candidates.size();i++){
+            if(i>n && candidates[i] == candidates[i-1])
+                continue;
+            if(candidates[i] > target)
+                break;
+            current.push_back(candidates[i]);
+            helper(candidates,target - candidates[i],combinations,current,i+1);
+            current.pop_back();
+        }
+    }
+public:
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(),candidates.end());
+        vector<vector<int>> res;
+        vector<int> current;
+        helper(candidates,target,res,current,0);
+        return res;
+    }
+};
