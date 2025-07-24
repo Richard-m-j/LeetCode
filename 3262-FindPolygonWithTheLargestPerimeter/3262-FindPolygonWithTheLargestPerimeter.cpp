92609d1070ee30e07768c3e427f97b1a2b60b55d
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:22:44 +0530

    Create 3262-FindPolygonWithTheLargestPerimeter

---
+++ 3262-FindPolygonWithTheLargestPerimeter/3262-FindPolygonWithTheLargestPerimeter.cpp
@@ -0,0 +1,19 @@
+// Last updated: 7/24/2025, 8:22:44 AM
+class Solution {
+public:
+    long long largestPerimeter(vector<int>& nums) {
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+        sort(nums.begin(), nums.end());
+        long long sum = 0;
+
+        for (int i = nums.size() - 1; i >= 0; i--)
+            sum += nums[i];
+
+        for (int i = nums.size() - 1; i >= 0; i--)
+            if (nums[i] >= sum - nums[i])
+                sum -= nums[i];
+
+        return sum == 0? -1: sum;
+    }
+};
