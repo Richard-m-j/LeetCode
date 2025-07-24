6ad31f6d57867fb868574c47f396c9e9b01c591e
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:31:56 +0530

    Create 328-OddEvenLinkedList

---
+++ 328-OddEvenLinkedList/328-OddEvenLinkedList.cpp
@@ -0,0 +1,26 @@
+// Last updated: 7/24/2025, 8:31:56 AM
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+        if(!head || !head->next)
+            return head;
+
+        ListNode *odd =head;
+        ListNode *even = head->next;
+        ListNode *evenHead = even;
+
+        while(even && even->next)
+        {
+            odd->next=even->next;
+            odd=odd->next;
+            even->next=odd->next;
+            even=even->next;
+        }
+
+        odd->next=evenHead;
+        return head;
+    }
+};
