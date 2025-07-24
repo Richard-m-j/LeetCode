6e4224c90083fd670a042cfc5df109bc3cf36cad
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:35:16 +0530

    Create 66-PlusOne

---
+++ 66-PlusOne/66-PlusOne.cpp
@@ -0,0 +1,20 @@
+// Last updated: 7/24/2025, 8:35:16 AM
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        bool carry = 1;
+        for(int i=digits.size()-1;i>=0;i--)
+            if(digits[i] == 9)
+                digits[i] = 0;
+            else{
+                digits[i]++;
+                carry = 0;
+                break;
+            }
+        
+        if(carry)
+            digits.insert(digits.begin(),1);
+        
+        return digits;
+    }
+};
