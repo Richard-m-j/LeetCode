81bcabd8a6e42d8ca4abef7d77026d16e0d3e727
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:26:14 +0530

    Create 1737-MaximumNestingDepthOfTheParentheses

---
+++ 1737-MaximumNestingDepthOfTheParentheses/1737-MaximumNestingDepthOfTheParentheses.cpp
@@ -0,0 +1,18 @@
+// Last updated: 7/24/2025, 8:26:14 AM
+class Solution {
+public:
+    int maxDepth(string s) {
+        if(s == "" || s.length() == 1)
+            return 0;
+        int depth=0,ans=0;
+        for(int i=0;i<s.length();i++){
+            if(s[i] == '('){
+                depth++;
+                ans =max(ans,depth);
+            }
+            if(s[i] == ')')
+                depth--;
+        }
+        return ans;
+    }
+};
