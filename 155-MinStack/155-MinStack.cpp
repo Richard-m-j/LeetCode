7f7427f7445df884a79bc3338a75aae9773d3f56
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:33:29 +0530

    Create 155-MinStack

---
+++ 155-MinStack/155-MinStack.cpp
@@ -0,0 +1,39 @@
+// Last updated: 7/24/2025, 8:33:29 AM
+class MinStack {
+private:
+    vector<pair<int,int>> stackVector;
+public:
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        int minElement;
+        if(stackVector.empty())
+            minElement = val;
+        else 
+            minElement = min(val,stackVector[stackVector.size() - 1].second);
+        stackVector.push_back({val,minElement});
+    }
+    
+    void pop() {
+        stackVector.pop_back();
+    }
+    
+    int top() {
+        return stackVector[stackVector.size() - 1].first;
+    }
+    
+    int getMin() {
+        return stackVector[stackVector.size() - 1].second;
+    }
+};
+
+/**
+ * Your MinstackVector object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
