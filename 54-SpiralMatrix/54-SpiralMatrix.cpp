d97dd3887bda8e7156c8034c94fcc7a4d89faf91
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:35:31 +0530

    Create 54-SpiralMatrix

---
+++ 54-SpiralMatrix/54-SpiralMatrix.cpp
@@ -0,0 +1,33 @@
+// Last updated: 7/24/2025, 8:35:32 AM
+class Solution {
+private:
+    vector<pair<int,int>> dirs = {{0,1},{1,0},{0,-1},{-1,0}};
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+        int dir =0;
+        int m=matrix.size();
+        int n=matrix[0].size();
+
+        vector<int> res;
+        int x=0,y=0;
+        while(res.size()<m*n){
+            res.push_back(matrix[x][y]);
+            matrix[x][y] = 101;
+            int nextX = x + dirs[dir].first;
+            int nextY = y + dirs[dir].second;
+            if(nextX>=0 && nextY>=0 && nextX<m && nextY<n && matrix[nextX][nextY]!=101){
+                x=nextX;
+                y=nextY;
+            }
+            else{
+                dir = (dir+1)%4;
+                x = x + dirs[dir].first;
+                y = y + dirs[dir].second;
+            }
+        }
+        return res;
+    }
+};
