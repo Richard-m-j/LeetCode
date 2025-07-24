e48031b82277c3fb20373097c3cd2e46e7b378da
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:31:25 +0530

    Create 387-FirstUniqueCharacterInAString

---
+++ 387-FirstUniqueCharacterInAString/387-FirstUniqueCharacterInAString.cpp
@@ -0,0 +1,15 @@
+// Last updated: 7/24/2025, 8:31:25 AM
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        vector <int> mp(26,0);
+        for(char&c:s)
+            mp[c-'a']++;
+        for(int i =0;i<s.length();i++)
+            if(mp[s[i]-'a'] == 1)
+                return i;
+        return -1; 
+    }
+};
