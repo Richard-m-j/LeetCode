06ef6ce545813e279df896560cebfb2eb1d8486c
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:35:37 +0530

    Create 49-GroupAnagrams

---
+++ 49-GroupAnagrams/49-GroupAnagrams.py
@@ -0,0 +1,10 @@
+# Last updated: 7/24/2025, 8:35:38 AM
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        mp = defaultdict(list)
+        for word in strs:
+            chars = [0]*26
+            for c in word:
+                chars[ord(c)-97] += 1
+            mp[tuple(chars)].append(word)
+        return list(mp.values())
