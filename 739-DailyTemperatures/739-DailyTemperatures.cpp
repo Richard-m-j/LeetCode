5ceff6abf1111bb1abce4dc03a239a6c03f6b603
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:29:36 +0530

    Create 739-DailyTemperatures

---
+++ 739-DailyTemperatures/739-DailyTemperatures.cpp
@@ -0,0 +1,23 @@
+// Last updated: 7/24/2025, 8:29:37 AM
+static const int ZERO = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    return 0;
+}();
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        stack<int> indexStack;
+        vector<int> res(temperatures.size(),0);
+        for (int i = temperatures.size()-1; i >= 0; i--) 
+        {
+            while(!indexStack.empty() && temperatures[indexStack.top()] <= temperatures[i])
+                indexStack.pop();
+            if(!indexStack.empty())
+                res[i] = indexStack.top() - i;
+            
+            indexStack.push(i);
+        }
+        return res;
+    }
+};
