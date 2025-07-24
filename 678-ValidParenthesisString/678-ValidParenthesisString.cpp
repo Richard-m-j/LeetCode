d7685c7cdc6a0e61ae42b477496f425aa8f4d711
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:29:47 +0530

    Create 678-ValidParenthesisString

---
+++ 678-ValidParenthesisString/678-ValidParenthesisString.cpp
@@ -0,0 +1,17 @@
+// Last updated: 7/24/2025, 8:29:47 AM
+class Solution {
+public:
+        bool checkValidString(string s) {
+        int cmin = 0, cmax = 0;
+        for (char c : s) {
+            if (c == '(')
+                cmax++, cmin++;
+            if (c == ')')
+                cmax--, cmin = max(cmin - 1, 0);
+            if (c == '*')
+                cmax++, cmin = max(cmin - 1, 0);
+            if (cmax < 0) return false;
+        }
+        return cmin == 0;
+    }
+};
