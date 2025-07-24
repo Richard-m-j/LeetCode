6d36066da15ce742ad455f877422224188ea18cd
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:23:16 +0530

    Create 2820-ReturnLengthOfArgumentsPassed

---
+++ 2820-ReturnLengthOfArgumentsPassed/2820-ReturnLengthOfArgumentsPassed.js
@@ -0,0 +1,11 @@
+// Last updated: 7/24/2025, 8:23:16 AM
+/**
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    return args.length;
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
