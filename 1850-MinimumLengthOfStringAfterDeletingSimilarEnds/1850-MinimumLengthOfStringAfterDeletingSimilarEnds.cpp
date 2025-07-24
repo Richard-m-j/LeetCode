b4bdd24fcd23e4eff297fd12e55b357f08c9006c
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:25:49 +0530

    Create 1850-MinimumLengthOfStringAfterDeletingSimilarEnds

---
+++ 1850-MinimumLengthOfStringAfterDeletingSimilarEnds/1850-MinimumLengthOfStringAfterDeletingSimilarEnds.cpp
@@ -0,0 +1,20 @@
+// Last updated: 7/24/2025, 8:25:50 AM
+class Solution {
+public:
+    int minimumLength(string s) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        int left = 0, right = s.length() - 1;
+
+        while(left < right) {
+            if(s[left] == s[right]) {
+                int c = s[left];
+                while(left <= right && s[left] == c) left++;
+                while(left <= right && s[right] == c) right--;
+            }
+            else break;
+        }
+
+        return right - left + 1;
+    }
+};
