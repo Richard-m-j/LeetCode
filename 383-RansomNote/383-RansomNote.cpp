981d5909904daa6bf1ab5f062b9dba61ba52f70c
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:31:24 +0530

    Create 383-RansomNote

---
+++ 383-RansomNote/383-RansomNote.cpp
@@ -0,0 +1,15 @@
+// Last updated: 7/24/2025, 8:31:24 AM
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        vector<int> mp(26,0);
+        for(char& c: ransomNote)
+            mp[c-'a']++;
+        for(char& c: magazine)
+            mp[c-'a']--;
+        for(int i=0;i<26;i++)
+            if(mp[i]>0)
+                return false;
+        return true;
+    }
+};
