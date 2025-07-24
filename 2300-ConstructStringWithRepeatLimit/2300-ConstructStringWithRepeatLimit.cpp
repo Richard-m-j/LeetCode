4d974ff8ada4963d532f330bd717174c49548411
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:24:39 +0530

    Create 2300-ConstructStringWithRepeatLimit

---
+++ 2300-ConstructStringWithRepeatLimit/2300-ConstructStringWithRepeatLimit.cpp
@@ -0,0 +1,44 @@
+// Last updated: 7/24/2025, 8:24:39 AM
+class Solution {
+public:
+    string repeatLimitedString(string s, int repeatLimit) {
+        vector<int> freq(26,0);
+        for(char& c: s)
+            freq[c-'a']++;
+        string res="";
+        int i=25,prev=-1;
+        while(true){
+            int idx=-1;
+            for(i=25;i>=0;i--)
+                if(freq[i]){
+                    idx = i;
+                    break;
+                }
+            if(idx == -1)
+                break;
+            if(idx==prev){
+                // cout<<res<<" "<<idx<<" ";
+                for(i=idx-1;i>=0;i--)
+                    if(freq[i]){
+                        idx = i;
+                        break;
+                    }
+                if(i == -1){
+                    cout<<idx<<" ";
+                    break;}
+                char node = char('a'+idx);
+                res += node;
+                freq[idx]--;
+                prev = idx;
+                continue;
+            }
+            int l = min(repeatLimit,freq[idx]);
+            char node = char('a'+idx);
+            freq[idx] -= l;
+            for(int j=0;j<l;j++)
+                res+=node;
+            prev = idx;
+        }
+        return res;
+    }
+};
