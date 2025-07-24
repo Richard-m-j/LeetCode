93e35b6e92e15fe1716505c441cc8b8a224f3bca
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:25:13 +0530

    Create 2145-GridGame

---
+++ 2145-GridGame/2145-GridGame.cpp
@@ -0,0 +1,17 @@
+// Last updated: 7/24/2025, 8:25:14 AM
+class Solution {
+public:
+    long long gridGame(vector<vector<int>>& grid) {
+        int n = grid[0].size();
+        long long row1Sum = 0;
+        for(int& num: grid[0])
+            row1Sum += num;
+        long long row2Sum = 0,res=LLONG_MAX;
+        for(int i=0;i<n;i++){
+            row1Sum -= grid[0][i];
+            res = min(res,max(row1Sum,row2Sum));
+            row2Sum += grid[1][i];
+        }
+        return res;
+    }
+};
