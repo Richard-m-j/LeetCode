d30928316fe6546aa2ea3e4d41ae14c70e42d507
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:27:43 +0530

    Create 1256-RankTransformOfAnArray

---
+++ 1256-RankTransformOfAnArray/1256-RankTransformOfAnArray.cpp
@@ -0,0 +1,22 @@
+// Last updated: 7/24/2025, 8:27:43 AM
+class Solution {
+public:
+    vector<int> arrayRankTransform(vector<int>& arr) {
+        priority_queue<pair<int, int>> pq;
+        int n = arr.size();
+        for (int i = 0; i < n; i++) {
+            pq.push({-arr[i], i});
+        }
+        int i = 0;
+        int prev = INT_MAX;
+        while (!pq.empty()) {
+            auto it = pq.top();
+            pq.pop();
+            if (-it.first != prev)
+                i++;
+            prev = -it.first;
+            arr[it.second] = i;
+        }
+        return arr;
+    }
+};
