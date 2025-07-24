9f4a9c8a646503afe6b2bd661f0187573448c3b2
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:26:15 +0530

    Create 1731-EvenOddTree

---
+++ 1731-EvenOddTree/1731-EvenOddTree.cpp
@@ -0,0 +1,34 @@
+// Last updated: 7/24/2025, 8:26:16 AM
+class Solution {
+public:
+    bool isEvenOddTree(TreeNode* root) {
+        queue<TreeNode*> q;
+        q.push(root);
+        int level = 0;
+
+        while (!q.empty()) {
+            int size = q.size();
+            int prevValue = (level % 2 == 0) ? INT_MIN : INT_MAX;
+
+            for (int i = 0; i < size; ++i) {
+                TreeNode* node = q.front();
+                q.pop();
+
+                if ((level % 2 == 0 &&
+                     (node->val % 2 == 0 || node->val <= prevValue)) ||
+                    (level % 2 == 1 &&
+                     (node->val % 2 == 1 || node->val >= prevValue)))
+                    return false;
+
+                if (node->left)
+                    q.push(node->left);
+                if (node->right)
+                    q.push(node->right);
+
+                prevValue = node->val;
+            }
+            ++level;
+        }
+        return true;
+    }
+};
