524ec4e2536bb713723385a7565809c141f4f58c
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:35:50 +0530

    Create 42-TrappingRainWater

---
+++ 42-TrappingRainWater/42-TrappingRainWater.py
@@ -0,0 +1,19 @@
+# Last updated: 7/24/2025, 8:35:51 AM
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        leftMax, rightMax = 0, 0
+        rightMaxArr, leftMaxArr = [0] * len(height), [0] * len(height)
+        waterCanTrap = 0
+
+        for idx in range(len(height)):
+            leftMax = max(leftMax, height[idx])
+            leftMaxArr[idx] = leftMax
+
+        for idx in reversed(range(len(height))):
+            rightMax = max(rightMax, height[idx])
+            rightMaxArr[idx] = rightMax
+
+        for idx in range(len(height) - 1):
+            waterCanTrap += min(leftMaxArr[idx], rightMaxArr[idx]) - height[idx]
+        
+        return waterCanTrap
