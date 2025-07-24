6c75646681385e6e8fcd45c4936537337ba26d25
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:29:29 +0530

    Create 731-MyCalendarIi

---
+++ 731-MyCalendarIi/731-MyCalendarIi.cpp
@@ -0,0 +1,23 @@
+// Last updated: 7/24/2025, 8:29:29 AM
+class MyCalendarTwo 
+{
+    map<int, int> mp; // key: time; val: +1 if start, -1 if end
+public:
+    bool book(int start, int end) 
+    {
+        mp[start]++;
+        mp[end]--;
+        int booked = 0;
+        for (auto it = mp.begin(); it != mp.end(); it++) 
+        {
+            booked += it->second;
+            if (booked == 3) 
+            {
+                mp[start]--;
+                mp[end]++;
+                return false;
+            }
+        }
+        return true;
+    }
+};
