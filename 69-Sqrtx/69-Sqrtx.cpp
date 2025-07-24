068096b9556c87fd3562ead4265e62bb45ec64eb
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:35:13 +0530

    Create 69-Sqrtx

---
+++ 69-Sqrtx/69-Sqrtx.cpp
@@ -0,0 +1,23 @@
+// Last updated: 7/24/2025, 8:35:13 AM
+int speedUp = []{ios::sync_with_stdio(0);cin.tie(0);return 0;}();
+class Solution {
+public:
+    int mySqrt(int x) {
+        int l =0, r= x/2;
+        long m;
+        if(x == 1)
+            return 1;
+        while(l<=r)
+        {
+            m = l + (r-l)/2;
+            if(m*m == x)
+                return m;
+            else if(m*m < x)
+                l = m+1;
+            else
+                r = m-1;
+        }
+
+        return l-1;
+    }
+};
