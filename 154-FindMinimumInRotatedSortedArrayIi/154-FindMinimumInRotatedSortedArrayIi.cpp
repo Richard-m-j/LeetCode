ea46067f8e4601da506ad6fd3e9be8e22615062a
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:33:31 +0530

    Create 154-FindMinimumInRotatedSortedArrayIi

---
+++ 154-FindMinimumInRotatedSortedArrayIi/154-FindMinimumInRotatedSortedArrayIi.cpp
@@ -0,0 +1,32 @@
+// Last updated: 7/24/2025, 8:33:31 AM
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+        int l = 0, r= nums.size()-1,mini = nums[0];
+        while(l <= r)
+        {
+            int m = l + (r-l)/2;
+            mini = min(nums[m],mini);
+            if(nums[l] < nums[m])
+            {
+                mini = min(nums[l],mini);
+                l= m+1;
+            }
+            else if(nums[l] == nums[m])
+            {
+                l++;
+                if(nums[m] == nums[r])
+                    r--;
+            }
+            else
+            {
+                mini = min(nums[r],mini);
+                r= m-1;
+            }
+        }
+        return mini;
+    }
+};
