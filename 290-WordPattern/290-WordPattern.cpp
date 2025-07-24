6f0735d0cd12ecf4063dd61c84ab25db575c0e16
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:31:58 +0530

    Create 290-WordPattern

---
+++ 290-WordPattern/290-WordPattern.cpp
@@ -0,0 +1,39 @@
+// Last updated: 7/24/2025, 8:31:58 AM
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+        unordered_map<string, char> mp;
+        unordered_map<char, string> rmp;
+
+        vector<string> words;
+        int i = 0;
+        string word = "";
+        while (i < s.length()) {
+            if (s[i] == ' ') {
+                words.push_back(word);
+                word = "";
+            } else
+                word += s[i];
+            i++;
+        }
+        words.push_back(word);
+
+        if (words.size() != pattern.length())
+            return false;
+        for (int j = 0; j < words.size(); j++) {
+            if (!mp.count(words[j]) && !rmp.count(pattern[j])) {
+                mp[words[j]] = pattern[j];
+                rmp[pattern[j]] = words[j];
+            }
+            else if(!mp.count(words[j]) || !rmp.count(pattern[j]))
+                return false;
+            else {
+                if(mp[words[j]] != pattern[j] || rmp[pattern[j]] != words[j])
+                    return false;
+            }
+        }
+        return true;
+    }
+};
