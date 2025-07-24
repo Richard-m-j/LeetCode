3c9e26e71da9af437371ab35632ff7d669dbcba0
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:28:27 +0530

    Create 987-RevealCardsInIncreasingOrder

---
+++ 987-RevealCardsInIncreasingOrder/987-RevealCardsInIncreasingOrder.cpp
@@ -0,0 +1,20 @@
+// Last updated: 7/24/2025, 8:28:27 AM
+class Solution {
+public:
+    vector<int> deckRevealedIncreasing(vector<int>& deck) {
+  sort(begin(deck), end(deck));
+  list<int> l(deck.size());
+  iota(begin(l), end(l), 0);    
+  vector<int> res(deck.size());
+  auto lp = l.begin();
+  for (int i = 0, skip = 0; !l.empty(); skip = !skip) {
+      if (lp == l.end()) lp = l.begin();
+      if (skip) ++lp;
+      else {
+          res[*lp] = deck[i++];
+          l.erase(lp++);
+      }
+  }
+  return res;
+}
+};
