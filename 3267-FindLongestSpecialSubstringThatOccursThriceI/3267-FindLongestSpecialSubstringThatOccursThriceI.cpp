975ad3d9ee7a0cfb4ad9e123cc97106db7b4ef3d
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:22:42 +0530

    Create 3267-FindLongestSpecialSubstringThatOccursThriceI

---
+++ 3267-FindLongestSpecialSubstringThatOccursThriceI/3267-FindLongestSpecialSubstringThatOccursThriceI.cpp
@@ -0,0 +1,30 @@
+// Last updated: 7/24/2025, 8:22:43 AM
+class Solution {
+public:
+    int maximumLength(string s) {
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+        unordered_map<string,int> mp;
+        int n = s.length();
+        for(int i=0;i<n;i++){
+            string special="";
+            special+=s[i];
+            mp[special]++;
+            for(int j=i+1;j<n;j++){
+                if(s[j]!=special[0])
+                    break;
+                special += s[j];
+                mp[special]++;
+            }
+        }
+        int res = -1;
+        for(auto& it: mp){
+            // cout<<it.first<<" "<< it.second<<endl;
+            int len = (it.first).length();
+            if(it.second>2){
+                res = max(res,len);
+            }
+        }
+        return res;
+    }
+};
