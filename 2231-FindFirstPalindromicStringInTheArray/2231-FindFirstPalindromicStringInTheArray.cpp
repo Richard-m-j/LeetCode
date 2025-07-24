9e8f97db908d7744d8c3297e9e147dc3c33c6bf2
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:24:52 +0530

    Create 2231-FindFirstPalindromicStringInTheArray

---
+++ 2231-FindFirstPalindromicStringInTheArray/2231-FindFirstPalindromicStringInTheArray.cpp
@@ -0,0 +1,17 @@
+// Last updated: 7/24/2025, 8:24:52 AM
+class Solution {
+public:
+    string firstPalindrome(vector<string>& words) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        for (string& word : words) {
+            int l = 0, r = word.length() - 1;
+            for (; l <= r; l++, r--)
+                if (word[l] != word[r])
+                    break;
+            if (l > r)
+                return word;
+        }
+        return "";
+    }
+};
