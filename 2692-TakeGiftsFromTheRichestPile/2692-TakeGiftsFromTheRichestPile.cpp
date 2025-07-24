55a8a92a3f54dbe46a7e40fbbd207362f45891c6
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:23:27 +0530

    Create 2692-TakeGiftsFromTheRichestPile

---
+++ 2692-TakeGiftsFromTheRichestPile/2692-TakeGiftsFromTheRichestPile.cpp
@@ -0,0 +1,25 @@
+// Last updated: 7/24/2025, 8:23:28 AM
+class Solution {
+public:
+    long long pickGifts(vector<int>& gifts, int k) {
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+        priority_queue<int> pq;
+        for(int& gift: gifts)
+            pq.push(gift);
+        while(k && !pq.empty()){
+            int g = pq.top();
+            pq.pop();
+            int sqroot = sqrt(g);
+            pq.push(sqroot);
+            k--;
+        }
+        long long res=0;
+        while(!pq.empty()){
+            int g = pq.top();
+            pq.pop();
+            res += g;
+        }
+        return res;
+    }
+};
