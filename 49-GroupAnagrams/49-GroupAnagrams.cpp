a4411c6c08120b311c4c3f03b502b2ef30ad20d7
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:35:42 +0530

    Create 49-GroupAnagrams

---
+++ 49-GroupAnagrams/49-GroupAnagrams.cpp
@@ -0,0 +1,38 @@
+// Last updated: 7/24/2025, 8:35:42 AM
+class Solution {
+private:
+    struct VectorHash {
+        size_t operator()(const vector<int>& vec) const {
+            size_t hash = 0;
+            for (auto& i : vec) {
+                hash ^= i + 0x9e3779b9 + (hash << 6) + (hash >> 2);
+            }
+            return hash;
+        }
+    };
+
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        unordered_map<vector<int>, int, VectorHash> mp;
+        vector<vector<string>> res;
+        int index = 0;
+
+        for (const auto& str : strs) {
+            vector<int> mapping(26, 0);
+            for (char c : str) {
+                mapping[c - 'a']++;
+            }
+
+            if (mp.find(mapping) != mp.end()) {
+                res[mp[mapping]].push_back(str);
+            } else {
+                mp[mapping] = index++;
+                res.push_back({str});
+            }
+        }
+
+        return res;
+    }
+};
