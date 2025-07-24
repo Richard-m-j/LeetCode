88654c25148a9f658a44630aaf36717a0602bf0f
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:28:06 +0530

    Create 1063-BestSightseeingPair

---
+++ 1063-BestSightseeingPair/1063-BestSightseeingPair.cpp
@@ -0,0 +1,15 @@
+// Last updated: 7/24/2025, 8:28:06 AM
+class Solution {
+public:
+    int maxScoreSightseeingPair(vector<int>& values) {
+        int n = values.size();
+        vector<int> dp(n);
+        dp[0]=values[0];
+        int res = 0;
+        for(int i=1;i<n;i++){
+            dp[i]=max(values[i]+i,dp[i-1]);
+            res = max(res,dp[i-1]+values[i]-i);
+        }
+        return res;
+    }
+};
