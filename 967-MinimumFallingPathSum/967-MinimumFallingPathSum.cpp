ac69b9fba940b96fe67d1f6730f4c0a3cea9044e
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:28:31 +0530

    Create 967-MinimumFallingPathSum

---
+++ 967-MinimumFallingPathSum/967-MinimumFallingPathSum.cpp
@@ -0,0 +1,29 @@
+// Last updated: 7/24/2025, 8:28:32 AM
+int speedUp = []{ios::sync_with_stdio(0);cin.tie(0);return 0;}();
+class Solution {
+private:
+    int n;
+    vector<vector<int>> minSum;
+public:
+    int minFallingPathSum(vector<vector<int>>& matrix) 
+    {
+        n = matrix.size();
+        minSum.resize(n, vector<int>(n, INT_MAX));
+        int minimum = INT_MAX;
+        for(int i = 0; i < n; i++)
+            minimum = min(helper(matrix,0,i), minimum);
+        return minimum;
+    }
+    int helper(vector<vector<int>>& matrix,int i,int j)
+    {
+        if(i<0 || i==n || j<0 || j==n)
+            return INT_MAX;
+        if(minSum[i][j] != INT_MAX)
+            return minSum[i][j];
+        int recursion = min(min(helper(matrix,i+1,j-1),helper(matrix,i+1,j)),helper(matrix,i+1,j+1));
+        if(i == n-1)
+            recursion = 0;
+        minSum[i][j] = matrix[i][j] + recursion;
+        return minSum[i][j];
+    }
+};
