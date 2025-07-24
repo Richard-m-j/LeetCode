f6ec251089339f9cf3f41196afc28a2f7097afb7
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:22:48 +0530

    Create 3225-LengthOfLongestSubarrayWithAtMostKFrequency

---
+++ 3225-LengthOfLongestSubarrayWithAtMostKFrequency/3225-LengthOfLongestSubarrayWithAtMostKFrequency.cpp
@@ -0,0 +1,26 @@
+// Last updated: 7/24/2025, 8:22:48 AM
+class Solution {
+public:
+    int maxSubarrayLength(vector<int>& nums, int k) {
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+        unordered_map<int, int> freq;
+        int ans = 0;
+        bool flag = false;
+        int l = 0, r = 0, n = nums.size();
+        for (; r < n; r++) {
+            if (++freq[nums[r]] > k) {
+                flag = true;
+            }
+            while (flag && l <= r) {
+                freq[nums[l]]--;
+                ans = max(ans, r - l);
+                l++;
+                if(freq[nums[r]] == k) flag = false;
+            }
+        }
+        ans = max(ans, r - l);
+        
+        return ans;
+    }
+};
