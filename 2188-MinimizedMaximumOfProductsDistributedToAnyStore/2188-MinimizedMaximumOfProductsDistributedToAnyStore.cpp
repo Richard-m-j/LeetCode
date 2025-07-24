3eb8d69ae2f99cfceea9fc2427d8a46b9de3b9fb
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:25:04 +0530

    Create 2188-MinimizedMaximumOfProductsDistributedToAnyStore

---
+++ 2188-MinimizedMaximumOfProductsDistributedToAnyStore/2188-MinimizedMaximumOfProductsDistributedToAnyStore.cpp
@@ -0,0 +1,25 @@
+// Last updated: 7/24/2025, 8:25:05 AM
+class Solution {
+public:
+    int minimizedMaximum(int n, vector<int>& quantities) {
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+        // sort(quantities.begin(),quantities.end());
+        int l=1,r=*max_element(quantities.begin(),quantities.end());
+        while(l<=r){
+            int m = l + (r-l)/2;
+            int numDistributions = 0;
+            for(int i=0;i<quantities.size();i++){
+                numDistributions += ceil(quantities[i]/float(m));
+            }
+            // cout<<m<<" "<<numDistributions<<endl;
+            // if(numDistributions==n)
+            //     return m;
+            if(numDistributions<=n)
+                r=m-1;
+            else
+                l=m+1;
+        }
+        return l;
+    }
+};
