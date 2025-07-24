67870f690466be981e31d79dd3590537318a62b9
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:22:56 +0530

    Create 3174-MinimumNumberOfChangesToMakeBinaryStringBeautiful

---
+++ 3174-MinimumNumberOfChangesToMakeBinaryStringBeautiful/3174-MinimumNumberOfChangesToMakeBinaryStringBeautiful.cpp
@@ -0,0 +1,13 @@
+// Last updated: 7/24/2025, 8:22:56 AM
+class Solution {
+public:
+    int minChanges(string s) {
+        int n=s.length();
+        int res = 0;
+        for(int i=0;i<n-1;i+=2){
+            if((s[i]-'0')^(s[i+1]-'0'))
+                res++;
+        }
+        return res;
+    }
+};
