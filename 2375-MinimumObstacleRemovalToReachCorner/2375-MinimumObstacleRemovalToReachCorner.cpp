4575a7d49a951658027330068dd27e27fe53896b
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:24:31 +0530

    Create 2375-MinimumObstacleRemovalToReachCorner

---
+++ 2375-MinimumObstacleRemovalToReachCorner/2375-MinimumObstacleRemovalToReachCorner.cpp
@@ -0,0 +1,36 @@
+// Last updated: 7/24/2025, 8:24:32 AM
+class Solution {
+private:
+    vector<pair<int, int>> dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+public:
+    int minimumObstacles(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+
+        vector<vector<int>> dp(n, vector<int>(m, INT_MAX));
+
+        queue<pair<int, int>> q;
+        q.push({0, 0});
+        dp[0][0] = 0;
+        while (q.size()) {
+            int qLen = q.size();
+            for (int i = 0; i < qLen; i++) {
+                auto node = q.front();
+                q.pop();
+                int x = node.first;
+                int y = node.second;
+                for (auto& d : dirs) {
+                    int nx = x + d.first;
+                    int ny = y + d.second;
+                    if (nx >= 0 && ny >= 0 && nx < n && ny < m &&
+                        dp[x][y] + grid[nx][ny] <dp[nx][ny]) {
+                        dp[nx][ny] = dp[x][y] + grid[nx][ny];
+                        q.push({nx, ny});
+                    }
+                }
+            }
+        }
+        return dp[n - 1][m - 1];
+    }
+};
