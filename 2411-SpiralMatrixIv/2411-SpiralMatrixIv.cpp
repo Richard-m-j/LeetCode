80ade7473c06fc8d5c0bed0739440d3259e1d4fb
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:24:27 +0530

    Create 2411-SpiralMatrixIv

---
+++ 2411-SpiralMatrixIv/2411-SpiralMatrixIv.cpp
@@ -0,0 +1,42 @@
+// Last updated: 7/24/2025, 8:24:27 AM
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+private:
+    vector<pair<int,int>> dirs = {{0,1},{1,0},{0,-1},{-1,0}};
+public:
+    vector<vector<int>> spiralMatrix(int m, int n, ListNode* head) {
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+        vector<vector<int>> mat(m,vector<int>(n,-1));
+        ListNode*ptr = head;
+        int dir = 0;
+        int x=0,y=0;        
+        while(ptr){
+            mat[x][y] = ptr->val;
+            // cout<<x<<","<<y<<endl;
+            ptr = ptr->next;
+            int nextX = x + dirs[dir].first;
+            int nextY = y + dirs[dir].second;
+            if(nextX>=0 && nextY>=0 && nextX<m && nextY<n && mat[nextX][nextY] == -1){
+                // cout<<endl;
+                x = nextX;
+                y = nextY;
+            }
+            else{
+                dir = (dir+1)%4;
+                x += dirs[dir].first;
+                y += dirs[dir].second;
+            }
+        }
+        return mat;
+    }
+};
