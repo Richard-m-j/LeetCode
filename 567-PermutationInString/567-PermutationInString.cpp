5d451d83b330e94d48ad4a499f2bd47cf5105fb4
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:30:21 +0530

    Create 567-PermutationInString

---
+++ 567-PermutationInString/567-PermutationInString.cpp
@@ -0,0 +1,32 @@
+// Last updated: 7/24/2025, 8:30:21 AM
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+        int n = s1.length();
+        int m = s2.length();
+        if (n > m)
+            return false;
+        vector<int> mp1(26, 0);
+        vector<int> mp2(26, 0);
+        for (int i = 0; i < n; i++) {
+            mp1[s1[i] - 'a']++;
+        }
+        int numChars = 0,start=0;
+
+        for (int i = 0; i < m; i++) {
+            mp2[s2[i]-'a']++;
+            numChars++;
+            while(mp2[s2[i]-'a']>mp1[s2[i]-'a']){
+                mp2[s2[start]-'a']--;
+                start++;
+                numChars--;
+            }
+            cout<<numChars<<" ";
+            if(numChars==n)
+                return true;
+        }
+        return false;
+    }
+};
