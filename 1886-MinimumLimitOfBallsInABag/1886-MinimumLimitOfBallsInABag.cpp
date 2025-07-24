1bab03fda6a6e66c1b7de59ed9929416d3795cc0
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:25:48 +0530

    Create 1886-MinimumLimitOfBallsInABag

---
+++ 1886-MinimumLimitOfBallsInABag/1886-MinimumLimitOfBallsInABag.cpp
@@ -0,0 +1,22 @@
+// Last updated: 7/24/2025, 8:25:48 AM
+class Solution {
+public:
+    int minimumSize(vector<int>& nums, int maxOperations) {
+        int l=1,r=*max_element(nums.begin(),nums.end());
+        int n=nums.size();
+        while(l<r){
+            int m = l + (r-l)/2;
+            int numOps = 0;
+            for(int& num: nums){
+                numOps += (num-1)/m;
+                if(numOps>maxOperations)
+                    break;
+            }
+            if(numOps<=maxOperations)
+                r=m;
+            else
+                l=m+1;
+        }
+        return r;
+    }
+};
