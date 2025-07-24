64afeaededf274edef48b31387f6ca2becca8c7e
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:30:29 +0530

    Create 530-MinimumAbsoluteDifferenceInBst

---
+++ 530-MinimumAbsoluteDifferenceInBst/530-MinimumAbsoluteDifferenceInBst.cpp
@@ -0,0 +1,36 @@
+// Last updated: 7/24/2025, 8:30:30 AM
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+int speedUp = []{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    return 0;
+}();
+class Solution {
+private:
+    int res = INT_MAX;
+    int prev = -1;
+public:
+    int getMinimumDifference(TreeNode* root) {
+        inorder(root);
+        return res;
+    }
+    void inorder(TreeNode* root){
+        if(!root)
+            return;
+        inorder(root->left);
+        if(prev!=-1)
+            res = min(res,abs(root->val - prev));
+        prev = root->val;
+        inorder(root->right);
+    }
+};
