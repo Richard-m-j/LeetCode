d140836b3a3912c2357b2f15d152fa43a40f9f86
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:29:16 +0530

    Create 789-KthLargestElementInAStream

---
+++ 789-KthLargestElementInAStream/789-KthLargestElementInAStream.cpp
@@ -0,0 +1,28 @@
+// Last updated: 7/24/2025, 8:29:16 AM
+class KthLargest {
+private:
+    priority_queue<int, vector<int>, greater<int>> heap;
+    int size;
+public:
+    KthLargest(int k, vector<int>& nums) {
+        size = k;
+        for(auto &num:nums){
+            heap.push(num);
+            if(heap.size()>k)
+                heap.pop();
+        }
+    }
+    
+    int add(int val) {
+        heap.push(val);
+        if(heap.size()>size)
+            heap.pop();
+        return heap.top();
+    }
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
