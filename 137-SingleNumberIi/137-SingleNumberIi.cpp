49d9d0517c69b0b7c17bdf776f32ede3979992da
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:33:50 +0530

    Create 137-SingleNumberIi

---
+++ 137-SingleNumberIi/137-SingleNumberIi.cpp
@@ -0,0 +1,19 @@
+// Last updated: 7/24/2025, 8:33:50 AM
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+        int ans = 0;
+        for(int i=0;i<32;i++){
+            int bit  = 1<<i, count = 0;
+            for(int& num: nums){
+                if(num&bit)
+                    count++;
+            } 
+            if(count%3)
+                ans |= bit;
+        }
+        return ans;
+    }
+};
