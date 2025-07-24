63adf6787a1a1e75c04f286a33688b0b8d1b4ef1
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:36:53 +0530

    Create 3-LongestSubstringWithoutRepeatingCharacters

---
+++ 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.cpp
@@ -0,0 +1,22 @@
+// Last updated: 7/24/2025, 8:36:53 AM
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+        vector<int> lastIndex(128,-1);
+        int res = 0;
+
+        int l=-1,r=0;
+        while(r<s.length()){
+            if(lastIndex[s[r]] >=l)
+                l = lastIndex[s[r]];
+            res = max(res,r-l);
+            lastIndex[s[r]] = r;
+    
+            r++;
+        }
+        return res;
+    }
+};
