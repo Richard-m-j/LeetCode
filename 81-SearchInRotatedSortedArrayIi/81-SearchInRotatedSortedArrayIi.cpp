ecbe7cfba972d82bf363c88047616978f1de34f1
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:34:57 +0530

    Create 81-SearchInRotatedSortedArrayIi

---
+++ 81-SearchInRotatedSortedArrayIi/81-SearchInRotatedSortedArrayIi.cpp
@@ -0,0 +1,31 @@
+// Last updated: 7/24/2025, 8:34:57 AM
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        ios_base::sync_with_stdio(false);
+        int l{0}, r = nums.size() - 1, m;
+
+        while(l <= r) {
+            m = l+(r-l)/2;
+
+            if(target == nums[m]) return true;
+
+            if(nums[l] == nums[m] && nums[m] == nums[r]) {
+                l++;
+                r--;
+                continue;
+            }
+            
+            if(nums[l] <= nums[m]) {   //check if left half is sorted
+                if(nums[l] <= target && target < nums[m]) r = m - 1;
+                else l = m + 1;
+            }
+
+            if(nums[m] <= nums[r]) {   //check if right half is sorted
+                if(nums[r] >= target && target > nums[m]) l = m + 1;
+                else r = m - 1;
+            }
+        }
+        return false;
+    }
+};
