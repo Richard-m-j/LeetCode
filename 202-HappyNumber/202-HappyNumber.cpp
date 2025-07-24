6d3ab5b2697ccde212edcf140a0a4d9405595263
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:32:59 +0530

    Create 202-HappyNumber

---
+++ 202-HappyNumber/202-HappyNumber.cpp
@@ -0,0 +1,20 @@
+// Last updated: 7/24/2025, 8:33:00 AM
+class Solution {
+public:
+    bool isHappy(int n) {
+        unordered_set<int> occured;
+        while(n!=1){
+            if(occured.find(n)!=occured.end())
+                return false;
+            occured.insert(n);
+            int newNumber = 0;
+            while(n){
+                int digit = n%10;
+                newNumber+=digit*digit;
+                n/=10;
+            }
+            n=newNumber;
+        }
+        return true;
+    }
+};
