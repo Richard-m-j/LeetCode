5f6efd824d3a89db5e359241fa1fc05a6685e9c1
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:25:54 +0530

    Create 1823-DetermineIfStringHalvesAreAlike

---
+++ 1823-DetermineIfStringHalvesAreAlike/1823-DetermineIfStringHalvesAreAlike.cpp
@@ -0,0 +1,25 @@
+// Last updated: 7/24/2025, 8:25:54 AM
+int speedUp = [] { ios::sync_with_stdio(0); cin.tie(0); return 0; }();
+class Solution {
+private:
+    unordered_map <char,bool> vowels = {{'A',true},{'E',true},{'I',true},{'O',true},{'U',true},{'a',true},{'e',true},{'i',true},{'o',true},{'u',true}};
+
+public:
+    bool halvesAreAlike(string s) {
+        int l = 0, r= s.length()-1;
+        int numl = 0;
+        int numr = 0;
+        while(l<r)
+        {
+            if(vowels[s[l]])
+                numl++;
+            if(vowels[s[r]])
+                numr++;
+            l++;
+            r--;
+        }
+        if(numl == numr)
+            return true;
+        return false;
+    }
+};
