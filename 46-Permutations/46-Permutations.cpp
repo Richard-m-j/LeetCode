3444e86b7f0972f1ac43d35ee1d8d0577b1979b6
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:35:40 +0530

    Create 46-Permutations

---
+++ 46-Permutations/46-Permutations.cpp
@@ -0,0 +1,23 @@
+// Last updated: 7/24/2025, 8:35:41 AM
+class Solution {
+private:
+    vector<vector<int>> res;
+
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        int n = nums.size();
+        helper(nums, 0);
+        return res;
+    }
+    void helper(vector<int>& nums, int l) {
+        if (l == nums.size() - 1) {
+            res.push_back(nums);
+            return;
+        }
+        for (int i = l; i < nums.size(); i++) {
+            swap(nums[l], nums[i]);
+            helper(nums, l + 1);
+            swap(nums[l], nums[i]);
+        }
+    }
+};
