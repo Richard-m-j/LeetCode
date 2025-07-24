015887e1cd92e5e1acf624d52ef31a70a775680c
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:29:31 +0530

    Create 735-AsteroidCollision

---
+++ 735-AsteroidCollision/735-AsteroidCollision.cpp
@@ -0,0 +1,39 @@
+// Last updated: 7/24/2025, 8:29:31 AM
+class Solution {
+public:
+    std::vector<int> asteroidCollision(std::vector<int>& asteroids) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
+        std::vector<int> stack;
+
+        for (int asteroid : asteroids) {
+            bool collisionHandled = false;
+            
+            while (!stack.empty() && stack.back() > 0 && asteroid < 0) {
+                int top = stack.back();
+                stack.pop_back();
+                
+                if (-asteroid > top) {
+            // Current asteroid survives, continue checking with the next stack element.
+                    continue;
+                } else if (-asteroid == top) {
+                    // Both asteroids annihilate each other.
+                    collisionHandled = true;
+                    break;
+                } 
+                else {
+                    // Top asteroid survives, push it back, and continue checking.
+                    stack.push_back(top);
+                    collisionHandled = true;
+                    break;
+                }
+            }
+            if (!collisionHandled) {
+                stack.push_back(asteroid);
+            }
+        }
+
+        return stack;
+    }
+};
