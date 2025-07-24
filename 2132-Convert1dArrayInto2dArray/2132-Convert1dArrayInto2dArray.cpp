c6aceeea0e692c6c76808070b3bc274a789b8e64
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:25:15 +0530

    Create 2132-Convert1dArrayInto2dArray

---
+++ 2132-Convert1dArrayInto2dArray/2132-Convert1dArrayInto2dArray.cpp
@@ -0,0 +1,19 @@
+// Last updated: 7/24/2025, 8:25:15 AM
+class Solution {
+public:
+    vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+        if(original.size() != m*n)
+            return {};
+        vector<vector<int>> res;
+        for(int i=0;i<m;i++){
+            vector<int> row;
+            for(int j=0;j<n;j++){
+                row.push_back(original[i*n + j]);
+            }
+            res.push_back(row);
+        }
+        return res;
+    }
+};
