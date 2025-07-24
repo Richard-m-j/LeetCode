7d95a37b0cc836d9c85b9bb03887aa3164f3b0bf
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:30:39 +0530

    Create 494-TargetSum

---
+++ 494-TargetSum/494-TargetSum.cpp
@@ -0,0 +1,31 @@
+// Last updated: 7/24/2025, 8:30:39 AM
+class Solution {
+private:
+    vector<vector<int>> dp;
+
+    int helper(vector<int>& nums, int target, int idx, int sum, int offset) {
+        if (idx == nums.size()) {
+            return sum == target;
+        }
+        if (dp[idx][sum + offset] != -1)
+            return dp[idx][sum + offset];
+
+        int add = helper(nums, target, idx + 1, sum + nums[idx], offset);
+        int sub = helper(nums, target, idx + 1, sum - nums[idx], offset);
+
+        return dp[idx][sum + offset] = add + sub;
+    }
+
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int total = accumulate(nums.begin(), nums.end(), 0);
+
+        if (abs(target) > total)
+            return 0;
+
+        int offset = total;
+        dp.resize(nums.size(), vector<int>(2 * total + 1, -1));
+
+        return helper(nums, target, 0, 0, offset);
+    }
+};
