7db5e598900c13481ea04b9a29d931c23dd4d185
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:35:39 +0530

    Create 47-PermutationsIi

---
+++ 47-PermutationsIi/47-PermutationsIi.cpp
@@ -0,0 +1,36 @@
+// Last updated: 7/24/2025, 8:35:39 AM
+int speedUp = []{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    return 0;
+}();
+class Solution {
+private:
+    int n;
+    void helper(unordered_map<int,int>& mp,vector<vector<int>>& res,vector<int>& current){
+        if(current.size() == n)
+            res.push_back(current);
+        for(auto& it:mp){
+            if(it.second){
+                current.push_back(it.first);
+                // cout<<it.first<<" ";
+                mp[it.first]--;
+                helper(mp,res,current);
+                mp[it.first]++;
+                current.pop_back();
+            }
+        }
+    }
+public:
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        n = nums.size();
+        unordered_map<int,int> mp;
+        for(int& num: nums)
+            mp[num]++;
+        vector<vector<int>> res;
+        vector<int> current;
+        helper(mp,res,current);
+        return res;
+        
+    }
+};
