a94b0eec3b142df0b8f0d282b1acf2bd874bd206
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:34:14 +0530

    Create 116-PopulatingNextRightPointersInEachNode

---
+++ 116-PopulatingNextRightPointersInEachNode/116-PopulatingNextRightPointersInEachNode.cpp
@@ -0,0 +1,51 @@
+// Last updated: 7/24/2025, 8:34:15 AM
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+int speedUp = []{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    return 0;
+}();
+class Solution {
+public:
+    Node* connect(Node* root) {
+        //perform bfs
+        if(!root)
+            return root;
+        queue<Node*> q;
+        q.push(root);
+        while(!q.empty()){
+            queue<Node*> q1;
+            Node *prev = new Node();
+            while(!q.empty()){
+                Node *ptr = q.front();
+                q.pop();
+                prev->next = ptr;
+                // cout<<ptr->val<<" ";
+                prev = ptr;
+                if(ptr->left){
+                    q1.push(ptr->left);
+                    q1.push(ptr->right);
+                }
+            }
+            prev->next = nullptr;
+            q = q1;
+        }
+        return root;
+    }
+};
