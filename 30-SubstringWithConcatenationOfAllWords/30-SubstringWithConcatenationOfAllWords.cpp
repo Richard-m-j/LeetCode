7eb85d7a11a0e6d86f7d389e931f1f11a60ab37c
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:36:06 +0530

    Create 30-SubstringWithConcatenationOfAllWords

---
+++ 30-SubstringWithConcatenationOfAllWords/30-SubstringWithConcatenationOfAllWords.cpp
@@ -0,0 +1,51 @@
+// Last updated: 7/24/2025, 8:36:07 AM
+class Solution {
+public:
+    vector<int> findSubstring(string s, vector<string>& words) {
+        vector<int> res;
+        if (words.empty() || s.empty()) return res;
+        
+        unordered_map<string, int> wordMap;
+        int wordLen = words[0].length();
+        int wordCount = words.size();
+        int sequenceLen = wordLen * wordCount;
+        
+        if (s.length() < sequenceLen) return res;
+        
+        for (string &word : words) {
+            wordMap[word]++;
+        }
+
+        for (int i = 0; i < wordLen; ++i) {
+            int start = i;  
+            unordered_map<string, int> windowMap;
+            int count = 0;
+            
+            for (int j = i; j + wordLen <= s.length(); j += wordLen) {
+                string word = s.substr(j, wordLen);
+                
+                if (wordMap.find(word)!=wordMap.end()) {
+                    windowMap[word]++;
+                    count++;
+                    
+                    while (windowMap[word] > wordMap[word]) {
+                        string firstWord = s.substr(start, wordLen);
+                        windowMap[firstWord]--;
+                        start += wordLen;
+                        count--;
+                    }
+                    
+                    if (count == wordCount) {
+                        res.push_back(start);
+                    }
+                } else {
+                    windowMap.clear();
+                    count = 0;
+                    start = j + wordLen;
+                }
+            }
+        }
+        
+        return res;
+    }
+};
