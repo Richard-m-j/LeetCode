ef0c82aa248f8432a60441dac2ad377e17623b9d
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:33:15 +0530

    Create 189-RotateArray

---
+++ 189-RotateArray/189-RotateArray.cpp
@@ -0,0 +1,18 @@
+// Last updated: 7/24/2025, 8:33:16 AM
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+        vector<int> right;
+        int n = nums.size();
+        if(k>n)
+            k %= n;
+        for(int i=n-k;i<n;i++)
+            right.push_back(nums[i]),cout<<nums[i]<<" ";
+        for(int i=n-1;i>=k;i--)
+            nums[i] = nums[i-k],cout<<nums[i]<<" ";
+        for(int i=0;i<k;i++)
+            nums[i] = right[i];
+    }
+};
