6dd0854f2322d6a893e63c1160de05adc43fb166
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:26:20 +0530

    Create 1694-MakeSumDivisibleByP

---
+++ 1694-MakeSumDivisibleByP/1694-MakeSumDivisibleByP.cpp
@@ -0,0 +1,31 @@
+// Last updated: 7/24/2025, 8:26:20 AM
+class Solution {
+public:
+    int minSubarray(vector<int>& nums, int p) {
+        long totalSum = 0;
+        for (int num : nums) {
+            totalSum += num;
+        }
+
+        int rem = totalSum % p;
+        if (rem == 0) return 0; 
+        unordered_map<int, int> prefixMod;
+        prefixMod[0] = -1;  
+        long prefixSum = 0;
+        int minLength = nums.size();
+
+        for (int i = 0; i < nums.size(); ++i) {
+            prefixSum += nums[i];
+            int currentMod = prefixSum % p;
+            int targetMod = (currentMod - rem + p) % p;
+
+            if (prefixMod.count(targetMod)) {
+                minLength = min(minLength, i - prefixMod[targetMod]);
+            }
+
+            prefixMod[currentMod] = i;
+        }
+
+        return minLength == nums.size() ? -1 : minLength;
+    }
+};
