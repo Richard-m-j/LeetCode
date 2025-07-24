515108b61e5cb4d22a12a034c59434aa615a772d
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:31:04 +0530

    Create 416-PartitionEqualSubsetSum

---
+++ 416-PartitionEqualSubsetSum/416-PartitionEqualSubsetSum.cpp
@@ -0,0 +1,28 @@
+// Last updated: 7/24/2025, 8:31:04 AM
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+        unordered_set<int> sumsSet;
+        vector<int> sumsVector;
+        long long totalSum = accumulate(nums.begin(),nums.end(),0);
+        if(totalSum%2)
+            return false;
+        long long target = totalSum/2;
+        sumsSet.insert(0);
+        sumsVector.push_back(0);
+        for(int i=0;i<nums.size();i++){
+            int len = sumsVector.size();
+            for(int j=0;j<len;j++){
+                if(sumsVector[j]+nums[i] == target)
+                    return true;
+                if(!sumsSet.count(sumsVector[j]+nums[i])){
+                    sumsVector.push_back(sumsVector[j] + nums[i]);
+                    sumsSet.insert(sumsVector[j] + nums[i]);
+                }
+            }
+        }
+        return false;
+    }
+};
