7067b698476ed05b65a3f7844ddf77a3092e4494
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:29:51 +0530

    Create 649-Dota2Senate

---
+++ 649-Dota2Senate/649-Dota2Senate.py
@@ -0,0 +1,25 @@
+# Last updated: 7/24/2025, 8:29:51 AM
+class Solution(object):
+    def predictPartyVictory(self, senate): 
+        l = len(senate)
+        banR = 0
+        banD = 0
+        
+        while 'R' in senate and 'D' in senate:
+            new_senate = []
+            for senator in senate:
+                if senator == 'R':
+                    if banR > 0:
+                        banR -= 1
+                    else:
+                        banD += 1
+                        new_senate.append('R')
+                else:  # senator == 'D'
+                    if banD > 0:
+                        banD -= 1
+                    else:
+                        banR += 1
+                        new_senate.append('D')
+            senate = new_senate
+        
+        return "Radiant" if 'R' in senate else "Dire"
