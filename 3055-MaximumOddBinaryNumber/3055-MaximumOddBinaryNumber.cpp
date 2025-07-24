d7a9635e50e6338af4cc846024628157cd4e5d3d
Richard Joseph <98447111+Richard-m-j@users.noreply.github.com>
2025-07-24 08:23:00 +0530

    Create 3055-MaximumOddBinaryNumber

---
+++ 3055-MaximumOddBinaryNumber/3055-MaximumOddBinaryNumber.cpp
@@ -0,0 +1,16 @@
+// Last updated: 7/24/2025, 8:23:00 AM
+class Solution {
+public:
+    string maximumOddBinaryNumber(string s) {
+        int n = s.length();
+        int numof1 = count(s.begin(), s.end(), '1');
+
+        string ans;
+        for(int i=0;i<numof1 - 1;i++)
+            ans += "1";
+        for(int i=0;i<n-numof1;i++)
+            ans+= "0";
+        ans += "1";
+        return ans;
+    }
+};
